02f3aec483b4c192fa996712282649311bd316bd
tianlinzhong <673359306@qq.com>
2021-03-02 13:46:22 +0800

    added shopfloorcost exporper

---
+++ myems-api/excelexporters/shopfloorcost.py
@@ -0,0 +1,12 @@
+import base64
+import uuid
+import os
+from openpyxl.chart import (
+        PieChart,
+        BarChart,
+        Reference,
+    )
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
+from openpyxl.drawing.image import Image
+from openpyxl import Workbook
+from openpyxl.chart.label import DataLabelList
