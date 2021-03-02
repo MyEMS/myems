28ae913f03e0dd003983cae84f7dbcc79ab9a58d
tianlinzhong <673359306@qq.com>
2021-03-02 13:55:21 +0800

    added shopfloorcost exporper

--- myems-api/excelexporters/shopfloorcost.py
+++ myems-api/excelexporters/shopfloorcost.py
@@ -10,3 +10,155 @@ from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
 from openpyxl.drawing.image import Image
 from openpyxl import Workbook
 from openpyxl.chart.label import DataLabelList
+
+
+####################################################################################################################
+# PROCEDURES
+# Step 1: Validate the report data
+# Step 2: Generate excel file
+# Step 3: Encode the excel file bytes to Base64
+####################################################################################################################
+
+
+def export(report,
+           name,
+           reporting_start_datetime_local,
+           reporting_end_datetime_local,
+           period_type):
+    ####################################################################################################################
+    # Step 1: Validate the report data
+    ####################################################################################################################
+    if report is None:
+        return None
+    print(report)
+
+    ####################################################################################################################
+    # Step 2: Generate excel file from the report data
+    ####################################################################################################################
+    filename = generate_excel(report,
+                              name,
+                              reporting_start_datetime_local,
+                              reporting_end_datetime_local,
+                              period_type)
+    ####################################################################################################################
+    # Step 3: Encode the excel file to Base64
+    ####################################################################################################################
+    try:
+        with open(filename, 'rb') as binary_file:
+            binary_file_data = binary_file.read()
+    except IOError as ex:
+        pass
+
+    # Base64 encode the bytes
+    base64_encoded_data = base64.b64encode(binary_file_data)
+    # get the Base64 encoded data using human-readable characters.
+    base64_message = base64_encoded_data.decode('utf-8')
+    # delete the file from server
+    try:
+        os.remove(filename)
+    except NotImplementedError as ex:
+        pass
+    return base64_message
+
+
+def generate_excel(report,
+                   name,
+                   reporting_start_datetime_local,
+                   reporting_end_datetime_local,
+                   period_type):
+    wb = Workbook()
+    ws = wb.active
+
+    # Row height
+    ws.row_dimensions[1].height = 102
+    for i in range(2, 2000 + 1):
+        ws.row_dimensions[i].height = 42
+
+    # for i in range(2, 37 + 1):
+    #     ws.row_dimensions[i].height = 30
+    #
+    # for i in range(38, 69 + 1):
+    #     ws.row_dimensions[i].height = 30
+
+    # Col width
+    ws.column_dimensions['A'].width = 1.5
+
+    ws.column_dimensions['B'].width = 25.0
+
+    for i in range(ord('C'), ord('L')):
+        ws.column_dimensions[chr(i)].width = 15.0
+
+    # Font
+    name_font = Font(name='Constantia', size=15, bold=True)
+    title_font = Font(name='宋体', size=15, bold=True)
+    data_font = Font(name='Franklin Gothic Book', size=11)
+
+    table_fill = PatternFill(fill_type='solid', fgColor='1F497D')
+    f_border = Border(left=Side(border_style='medium', color='00000000'),
+                      right=Side(border_style='medium', color='00000000'),
+                      bottom=Side(border_style='medium', color='00000000'),
+                      top=Side(border_style='medium', color='00000000')
+                      )
+    b_border = Border(
+        bottom=Side(border_style='medium', color='00000000'),
+    )
+
+    b_c_alignment = Alignment(vertical='bottom',
+                              horizontal='center',
+                              text_rotation=0,
+                              wrap_text=True,
+                              shrink_to_fit=False,
+                              indent=0)
+    c_c_alignment = Alignment(vertical='center',
+                              horizontal='center',
+                              text_rotation=0,
+                              wrap_text=True,
+                              shrink_to_fit=False,
+                              indent=0)
+    b_r_alignment = Alignment(vertical='bottom',
+                              horizontal='right',
+                              text_rotation=0,
+                              wrap_text=True,
+                              shrink_to_fit=False,
+                              indent=0)
+    c_r_alignment = Alignment(vertical='bottom',
+                              horizontal='center',
+                              text_rotation=0,
+                              wrap_text=True,
+                              shrink_to_fit=False,
+                              indent=0)
+
+    # Img
+    img = Image("excelexporters/myems.png")
+    img.width = img.width * 0.85
+    img.height = img.height * 0.85
+    # img = Image("myems.png")
+    ws.add_image(img, 'B1')
+
+    # Title
+    ws.row_dimensions[3].height = 60
+
+    ws['B3'].font = name_font
+    ws['B3'].alignment = b_r_alignment
+    ws['B3'] = 'Name:'
+    ws['C3'].border = b_border
+    ws['C3'].alignment = b_c_alignment
+    ws['C3'].font = name_font
+    ws['C3'] = name
+
+    ws['D3'].font = name_font
+    ws['D3'].alignment = b_r_alignment
+    ws['D3'] = 'Period:'
+    ws['E3'].border = b_border
+    ws['E3'].alignment = b_c_alignment
+    ws['E3'].font = name_font
+    ws['E3'] = period_type
+
+    ws['F3'].font = name_font
+    ws['F3'].alignment = b_r_alignment
+    ws['F3'] = 'Date:'
+    ws['G3'].border = b_border
+    ws['G3'].alignment = b_c_alignment
+    ws['G3'].font = name_font
+    ws['G3'] = reporting_start_datetime_local + "__" + reporting_end_datetime_local
+    ws.merge_cells("G3:H3")
