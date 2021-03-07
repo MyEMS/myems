38d98a5ca17c41e0ed1de2c52748d3d2d5e39b34
13621160019@163.com <13621160019@163.com>
2021-03-07 15:29:57 +0800

    updated change log and version number

--- myems-api/core/version.py
+++ myems-api/core/version.py
@@ -14,8 +14,8 @@ class VersionItem:
     @staticmethod
     def on_get(req, resp):
 
-        result = {"version": 'MyEMS 1.0.6 (Community Edition)',
-                  "release-date": '20210227',
+        result = {"version": 'MyEMS 1.0.7 (Community Edition)',
+                  "release-date": '20210307',
                   "website": "https://myems.io"}
         resp.body = json.dumps(result)
 
