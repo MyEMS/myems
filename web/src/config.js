bc371f9a4404371caa8bb1973405eccf923ba967
Caozhenhui <823914102@qq.com>
2021-03-08 16:56:32 +0800

    Implemented the CombinedEquipmentStatistics Excel Exporter

--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,5 @@
 export const version = '1.0.7';
 export const navbarBreakPoint = 'xl'; // Vertical navbar breakpoint
 export const topNavbarBreakpoint = 'lg';
-export const APIBaseURL = 'http://127.0.0.1:8000';
-//export const APIBaseURL = window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api";
+//export const APIBaseURL = 'http://127.0.0.1:8000';
+export const APIBaseURL = window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api";
