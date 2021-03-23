6ee35da206bd90b6d1b149cd2520166e4d021d32
13621160019@163.com <13621160019@163.com>
2021-03-23 17:46:54 +0800

    added AssociatedEquipmentTable to CombinedEquipmentEnergyItem Report in web UI

---
+++ web/src/components/MyEMS/common/AssociatedEquipmentTable.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import FalconCardHeader from '../../common/FalconCardHeader';
+import ButtonIcon from '../../common/ButtonIcon';
+import { Card, CardBody, CardHeader, Row, Col } from 'reactstrap';
+import { withTranslation } from 'react-i18next';
+
+
+const defaultSorted = [{
+  dataField: 'name',
+  order: 'asc'
+}];
+
+const AssociatedEquipmentTable = ({ title, data, columns, t }) => {
+  return (
+    <Fragment>
+      <Card>
+        <FalconCardHeader title={title} className="bg-light">
+          
+        </FalconCardHeader>
+        <CardBody>
+          <Row>
+            <Col>
+              <BootstrapTable
+                bootstrap4
+                keyField="id"
+                data={data}
+                columns={columns}
+                defaultSorted={defaultSorted}
+              />
+            </Col>
+          </Row>
+        </CardBody>
+      </Card>
+    </Fragment>
+  );
+};
+
+export default withTranslation()(AssociatedEquipmentTable);
