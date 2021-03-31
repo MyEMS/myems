e76066b62a8d2edc2700a04ee122c42b2c28b134
13621160019@163.com <13621160019@163.com>
2021-03-31 15:49:23 +0800

    upgraded version to v1.1.1

--- database/upgrade1.1.1.sql
+++ database/upgrade1.1.1.sql
@@ -1,6 +1,6 @@
 
 -- UPDATE VERSION NUMBER
-UPDATE myems_system_db.tbl_versions SET version='1.1.1', release_date='2021-03-22' WHERE id=1;
+UPDATE myems_system_db.tbl_versions SET version='1.1.1', release_date='2021-03-31' WHERE id=1;
 
 ALTER TABLE myems_fdd_db.tbl_rules ADD last_run_datetime_utc DATETIME NULL;
 ALTER TABLE myems_fdd_db.tbl_rules ADD next_run_datetime_utc DATETIME NULL;
