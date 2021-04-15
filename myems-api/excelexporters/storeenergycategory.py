cd94b8307c865f639b1ac9d97183b55f338124c3
13621160019@163.com <13621160019@163.com>
2021-04-15 11:19:17 +0800

    added associated parameters data to excel exporter of storeenergycategory in API

--- myems-api/excelexporters/storeenergycategory.py
+++ myems-api/excelexporters/storeenergycategory.py
@@ -4,13 +4,13 @@ import os
 from openpyxl.chart import (
         PieChart,
         LineChart,
-        BarChart,
         Reference,
     )
 from openpyxl.styles import PatternFill, Border, Side, Alignment, Font
 from openpyxl.drawing.image import Image
 from openpyxl import Workbook
 from openpyxl.chart.label import DataLabelList
+import openpyxl.utils.cell as format_cell
 
 ####################################################################################################################
 # PROCEDURES
@@ -59,6 +59,52 @@ def export(report,
     except NotImplementedError as ex:
         pass
     return base64_message
+
+
+def decimal_to_column(num=65):
+    string = ''
+    num = num - 64
+    # The column number is not greater than 90
+    if num <= 26:
+        return chr(num + 64)
+    # The column number is greater than 90
+    while num // 26 > 0:
+        if num % 26 == 0:
+            string += 'Z'
+            num = num // 26 - 1
+        else:
+            string += chr(num % 26 + 64)
+            num //= 26
+    # Avoid conversion errors that might occur between 741 and 766
+    if num > 0:
+        string += chr(num + 64)
+
+    return string[::-1]
+
+
+def column_to_decimal(string='A'):
+    num = 0
+    for index, key in enumerate(string[::-1]):
+        num += (ord(key) - 64) * (26 ** index)
+
+    return num + 64
+
+
+def get_parameters_timestamps_lists_max_len(parameters_timestamps_lists):
+    max_len = 0
+    for i, value in enumerate(list(parameters_timestamps_lists)):
+        if len(value) > max_len:
+            max_len = len(value)
+
+    return max_len
+
+
+def timestamps_data_all_equal_0(lists):
+    for i, value in enumerate(list(lists)):
+        if len(value) > 0:
+            return False
+
+    return True
 
 
 def generate_excel(report,
@@ -377,106 +423,6 @@ def generate_excel(report,
     else:
         for i in range(12, 18 + 1):
             ws.row_dimensions[i].height = 0.1
-        # end_row 10
-        # start_row 12
-    ################################################
-    # Third: 子空间能耗
-    # 19: title
-    # 20: table title
-    # 21~24 table_data
-    # Total: 6 rows
-    ################################################
-    has_child_flag = True
-    # Judge if the space has child space, if not, delete it.
-    if "child_space" not in report.keys() or "energy_category_names" not in report['child_space'].keys() or \
-            len(report['child_space']["energy_category_names"]) == 0 \
-            or 'child_space_names_array' not in report['child_space'].keys() \
-            or report['child_space']['energy_category_names'] is None \
-            or len(report['child_space']['child_space_names_array']) == 0 \
-            or len(report['child_space']['child_space_nchild_space_names_arrayames_array'][0]) == 0:
-
-        has_child_flag = False
-
-    current_row_number = 19
-
-    if has_child_flag:
-        child = report['child_space']
-        child_spaces = child['child_space_names_array'][0]
-        child_subtotals = child['subtotals_array'][0]
-
-        ws['B19'].font = title_font
-        ws['B19'] = name+' 子空间能耗'
-
-        ws.row_dimensions[20].height = 60
-        ws['B20'].fill = table_fill
-        ws['B20'].border = f_border
-        ca_len = len(child['energy_category_names'])
-
-        table_start_row_number = 20
-
-        for i in range(0, ca_len):
-            row = chr(ord('C') + i)
-            ws[row + '20'].fill = table_fill
-            ws[row + '20'].font = title_font
-            ws[row + '20'].alignment = c_c_alignment
-            ws[row + '20'].border = f_border
-            ws[row + '20'] = child['energy_category_names'][i] + ' (' + child['units'][i] + ')'
-
-        space_len = len(child['child_space_names_array'][0])
-
-        for i in range(0, space_len):
-            row = str(i + 21)
-
-            ws['B' + row].font = name_font
-            ws['B' + row].alignment = c_c_alignment
-            ws['B' + row] = child['child_space_names_array'][0][i]
-            ws['B' + row].border = f_border
-
-            for j in range(0, ca_len):
-                col = chr(ord('C') + j)
-                ws[col + row].font = name_font
-                ws[col + row].alignment = c_c_alignment
-                ws[col + row] = round(child['subtotals_array'][j][i], 2)
-                ws[col + row].border = f_border
-
-        table_end_row_number = 20 + space_len
-        chart_start_row_number = 20 + space_len + 1
-
-        for i in range(0, ca_len):
-            # pie
-            # 25~30: pie
-            pie = PieChart()
-            pie.title = ws.cell(column=3 + i, row=table_start_row_number).value
-            labels = Reference(ws, min_col=2, min_row=table_start_row_number + 1, max_row=table_end_row_number)
-            pie_data = Reference(ws, min_col=3 + i, min_row=table_start_row_number, max_row=table_end_row_number)
-            pie.add_data(pie_data, titles_from_data=True)
-            pie.set_categories(labels)
-            pie.height = 6.6  # cm 1.05*5 1.05cm = 30 pt
-            pie.width = 8
-            # pie.title = "Pies sold by category"
-            s1 = pie.series[0]
-            s1.dLbls = DataLabelList()
-            s1.dLbls.showCatName = False  # 标签显示
-            s1.dLbls.showVal = True  # 数量显示
-            s1.dLbls.showPercent = True  # 百分比显示
-            # s1 = CharacterProperties(sz=1800)     # 图表中字体大小 *100
-            chart_cell = ''
-            if i % 2 == 0:
-                chart_cell = 'B' + str(chart_start_row_number)
-            else:
-                chart_cell = 'E' + str(chart_start_row_number)
-                chart_start_row_number += 5
-            # ws.add_chart(pie, chart_cell)
-            # chart_col = chr(ord('B') + 2 * j)
-            # chart_cell = chart_col + '25'
-            ws.add_chart(pie, chart_cell)
-
-        current_row_number = chart_start_row_number
-
-        if ca_len % 2 == 1:
-            current_row_number += 5
-
-        current_row_number += 1
 
     ################################################
     # Fourth: 能耗详情
@@ -485,11 +431,18 @@ def generate_excel(report,
     # current_row_number+1+ca_len*6: table title
     # current_row_number+1+ca_len*6~: table_data
     ################################################
+    current_row_number = 19
     reporting_period_data = report['reporting_period']
     times = reporting_period_data['timestamps']
     has_detail_data_flag = True
     ca_len = len(report['reporting_period']['names'])
-    table_row = current_row_number + 1 + ca_len*6
+    parameters_names_len = len(report['parameters']['names'])
+    parameters_parameters_datas_len = 0
+    for i in range(0, parameters_names_len):
+        if len(report['parameters']['timestamps'][i]) == 0:
+            continue
+        parameters_parameters_datas_len += 1
+    table_row = current_row_number + (ca_len + parameters_parameters_datas_len) * 6 + 2
     chart_start_row_number = current_row_number + 1
     if "timestamps" not in reporting_period_data.keys() or \
             reporting_period_data['timestamps'] is None or \
@@ -585,6 +538,192 @@ def generate_excel(report,
                 chart_col = 'B'
                 chart_cell = chart_col + str(chart_start_row_number + 6*i)
                 ws.add_chart(line, chart_cell)
+
+    ##########################################
+    has_parameters_names_and_timestamps_and_values_data = True
+    # 12 is the starting line number of the last line chart in the report period
+
+    ca_len = len(report['reporting_period']['names'])
+    current_sheet_parameters_row_number = chart_start_row_number + ca_len * 6
+    if 'parameters' not in report.keys() or \
+            report['parameters'] is None or \
+            'names' not in report['parameters'].keys() or \
+            report['parameters']['names'] is None or \
+            len(report['parameters']['names']) == 0 or \
+            'timestamps' not in report['parameters'].keys() or \
+            report['parameters']['timestamps'] is None or \
+            len(report['parameters']['timestamps']) == 0 or \
+            'values' not in report['parameters'].keys() or \
+            report['parameters']['values'] is None or \
+            len(report['parameters']['values']) == 0 or \
+            timestamps_data_all_equal_0(report['parameters']['timestamps']):
+        has_parameters_names_and_timestamps_and_values_data = False
+    if has_parameters_names_and_timestamps_and_values_data:
+
+        ###############################
+        # new worksheet
+        ###############################
+
+        parameters_data = report['parameters']
+
+        parameters_names_len = len(parameters_data['names'])
+
+        parameters_ws = wb.create_sheet('相关参数')
+
+        parameters_timestamps_data_max_len = \
+            get_parameters_timestamps_lists_max_len(list(parameters_data['timestamps']))
+
+        # Row height
+        parameters_ws.row_dimensions[1].height = 102
+        for i in range(2, 7 + 1):
+            parameters_ws.row_dimensions[i].height = 42
+
+        for i in range(8, parameters_timestamps_data_max_len + 10):
+            parameters_ws.row_dimensions[i].height = 60
+
+        # Col width
+        parameters_ws.column_dimensions['A'].width = 1.5
+
+        parameters_ws.column_dimensions['B'].width = 25.0
+
+        for i in range(3, 12 + parameters_names_len * 3):
+            parameters_ws.column_dimensions[format_cell.get_column_letter(i)].width = 15.0
+
+        # Img
+        img = Image("excelexporters/myems.png")
+        img.width = img.width * 0.85
+        img.height = img.height * 0.85
+        # img = Image("myems.png")
+        parameters_ws.add_image(img, 'B1')
+
+        # Title
+        parameters_ws.row_dimensions[3].height = 60
+
+        parameters_ws['B3'].font = name_font
+        parameters_ws['B3'].alignment = b_r_alignment
+        parameters_ws['B3'] = 'Name:'
+        parameters_ws['C3'].border = b_border
+        parameters_ws['C3'].alignment = b_c_alignment
+        parameters_ws['C3'].font = name_font
+        parameters_ws['C3'] = name
+
+        parameters_ws['D3'].font = name_font
+        parameters_ws['D3'].alignment = b_r_alignment
+        parameters_ws['D3'] = 'Period:'
+        parameters_ws['E3'].border = b_border
+        parameters_ws['E3'].alignment = b_c_alignment
+        parameters_ws['E3'].font = name_font
+        parameters_ws['E3'] = period_type
+
+        parameters_ws['F3'].font = name_font
+        parameters_ws['F3'].alignment = b_r_alignment
+        parameters_ws['F3'] = 'Date:'
+        parameters_ws['G3'].border = b_border
+        parameters_ws['G3'].alignment = b_c_alignment
+        parameters_ws['G3'].font = name_font
+        parameters_ws['G3'] = reporting_start_datetime_local + "__" + reporting_end_datetime_local
+        parameters_ws.merge_cells("G3:H3")
+
+        parameters_ws_current_row_number = 6
+
+        parameters_ws['B' + str(parameters_ws_current_row_number)].font = title_font
+        parameters_ws['B' + str(parameters_ws_current_row_number)] = name + ' 相关参数'
+
+        parameters_ws_current_row_number += 1
+
+        parameters_table_start_row_number = parameters_ws_current_row_number
+
+        parameters_ws.row_dimensions[parameters_ws_current_row_number].height = 80
+
+        parameters_ws_current_row_number += 1
+
+        table_current_col_number = 'B'
+
+        for i in range(0, parameters_names_len):
+
+            if len(parameters_data['timestamps'][i]) == 0:
+                continue
+
+            parameters_ws[table_current_col_number + str(parameters_ws_current_row_number - 1)].fill = table_fill
+            parameters_ws[table_current_col_number + str(parameters_ws_current_row_number - 1)].border = f_border
+
+            col = decimal_to_column(column_to_decimal(table_current_col_number) + 1)
+
+            parameters_ws[col + str(parameters_ws_current_row_number - 1)].fill = table_fill
+            parameters_ws[col + str(parameters_ws_current_row_number - 1)].border = f_border
+            parameters_ws[col + str(parameters_ws_current_row_number - 1)].font = name_font
+            parameters_ws[col + str(parameters_ws_current_row_number - 1)].alignment = c_c_alignment
+            parameters_ws[col + str(parameters_ws_current_row_number - 1)] = parameters_data['names'][i]
+
+            table_current_row_number = parameters_ws_current_row_number
+
+            for j, value in enumerate(list(parameters_data['timestamps'][i])):
+                col = table_current_col_number
+
+                parameters_ws[col + str(table_current_row_number)].border = f_border
+                parameters_ws[col + str(table_current_row_number)].font = title_font
+                parameters_ws[col + str(table_current_row_number)].alignment = c_c_alignment
+                parameters_ws[col + str(table_current_row_number)] = value
+
+                col = decimal_to_column(column_to_decimal(col) + 1)
+
+                parameters_ws[col + str(table_current_row_number)].border = f_border
+                parameters_ws[col + str(table_current_row_number)].font = title_font
+                parameters_ws[col + str(table_current_row_number)].alignment = c_c_alignment
+                parameters_ws[col + str(table_current_row_number)] = round(parameters_data['values'][i][j], 2)
+
+                table_current_row_number += 1
+
+            table_current_col_number = decimal_to_column(column_to_decimal(table_current_col_number) + 3)
+
+        ########################################################
+        # parameters chart and parameters table
+        ########################################################
+
+        ws['B' + str(current_sheet_parameters_row_number)].font = title_font
+        ws['B' + str(current_sheet_parameters_row_number)] = name + ' 相关参数'
+
+        current_sheet_parameters_row_number += 1
+
+        chart_start_row_number = current_sheet_parameters_row_number
+
+        col_index = 0
+
+        for i in range(0, parameters_names_len):
+
+            if len(parameters_data['timestamps'][i]) == 0:
+                continue
+
+            line = LineChart()
+            data_col = 3 + col_index * 3
+            labels_col = 2 + col_index * 3
+            col_index += 1
+            line.title = '相关参数 - ' + \
+                         parameters_ws.cell(row=parameters_table_start_row_number, column=data_col).value
+            labels = Reference(parameters_ws, min_col=labels_col, min_row=parameters_table_start_row_number + 1,
+                               max_row=(len(parameters_data['timestamps'][i]) + parameters_table_start_row_number))
+            line_data = Reference(parameters_ws, min_col=data_col, min_row=parameters_table_start_row_number,
+                                  max_row=(len(parameters_data['timestamps'][i]) + parameters_table_start_row_number))
+            line.add_data(line_data, titles_from_data=True)
+            line.set_categories(labels)
+            line_data = line.series[0]
+            line_data.marker.symbol = "circle"
+            line_data.smooth = True
+            line.x_axis.crosses = 'min'
+            line.height = 8.25
+            line.width = 24
+            line.dLbls = DataLabelList()
+            line.dLbls.dLblPos = 't'
+            line.dLbls.showVal = False
+            line.dLbls.showPercent = False
+            chart_col = 'B'
+            chart_cell = chart_col + str(chart_start_row_number)
+            chart_start_row_number += 6
+            ws.add_chart(line, chart_cell)
+
+        current_sheet_parameters_row_number = chart_start_row_number
+
+        current_sheet_parameters_row_number += 1
 
     filename = str(uuid.uuid4()) + '.xlsx'
     wb.save(filename)
