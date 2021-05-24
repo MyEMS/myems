dca4a6da265cd92df2fafa3772d0393600e0f86b
13621160019@163.com <13621160019@163.com>
2021-05-24 18:27:47 +0800

    updated tbl_expressions in database

--- database/upgrade1.1.3.sql
+++ database/upgrade1.1.3.sql
@@ -2,6 +2,9 @@
 ALTER TABLE myems_system_db.tbl_points MODIFY COLUMN low_limit decimal(18,3) NOT NULL;
 ALTER TABLE myems_system_db.tbl_points ADD is_virtual BOOL DEFAULT FALSE NOT NULL AFTER is_trend;
 
+-- UPDATE EXPRESSION TABLE
+ALTER TABLE myems_system_db.tbl_expressions MODIFY COLUMN equation LONGTEXT NOT NULL;
+
 
 -- UPDATE VERSION NUMBER
 UPDATE myems_system_db.tbl_versions SET version='1.1.3', release_date='2021-05-05' WHERE id=1;
