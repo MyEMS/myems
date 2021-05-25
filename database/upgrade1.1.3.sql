db10874f192395148623ac67e6497f71420ee2ac
13621160019@163.com <13621160019@163.com>
2021-05-25 17:05:59 +0800

    updated version to v1.1.3

--- database/upgrade1.1.3.sql
+++ database/upgrade1.1.3.sql
@@ -7,4 +7,4 @@ ALTER TABLE myems_system_db.tbl_expressions MODIFY COLUMN equation LONGTEXT NOT 
 
 
 -- UPDATE VERSION NUMBER
-UPDATE myems_system_db.tbl_versions SET version='1.1.3', release_date='2021-05-05' WHERE id=1;
+UPDATE myems_system_db.tbl_versions SET version='1.1.3', release_date='2021-05-25' WHERE id=1;
