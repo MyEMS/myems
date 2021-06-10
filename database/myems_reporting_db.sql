15498b705b21a5a512a20c62e2656915180a78c5
13621160019@163.com <13621160019@163.com>
2021-06-10 14:39:01 +0800

    added tbl_email_messages to myems_reporting_db

--- database/myems_reporting_db.sql
+++ database/myems_reporting_db.sql
@@ -8,6 +8,25 @@ CREATE DATABASE IF NOT EXISTS `myems_reporting_db` ;
 USE `myems_reporting_db` ;
 
 -- ---------------------------------------------------------------------------------------------------------------------
+-- Table `myems_reporting_db`.`tbl_email_messages`
+-- ---------------------------------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS `myems_reporting_db`.`tbl_email_messages` ;
+
+CREATE TABLE IF NOT EXISTS `myems_reporting_db`.`tbl_email_messages` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `recipient_name` VARCHAR(128) NOT NULL,
+  `recipient_email` VARCHAR(128) NOT NULL,
+  `subject` VARCHAR(128) NOT NULL,
+  `message` LONGTEXT NOT NULL,
+  `attachment_file_name` VARCHAR(128) NULL,
+  `attachment_file_object` LONGBLOB NULL,
+  `created_datetime_utc` DATETIME NOT NULL,
+  `scheduled_datetime_utc` DATETIME NOT NULL,
+  `status` VARCHAR(32) NOT NULL COMMENT 'new, sent, timeout',
+  PRIMARY KEY (`id`));
+CREATE INDEX `tbl_email_messages_index_1` ON  `myems_reporting_db`.`tbl_email_messages`  (`status`,   `scheduled_datetime_utc`);
+
+-- ---------------------------------------------------------------------------------------------------------------------
 -- Table `myems_reporting_db`.`tbl_reports_files`
 -- ---------------------------------------------------------------------------------------------------------------------
 DROP TABLE IF EXISTS `myems_reporting_db`.`tbl_reports_files` ;
