e56f52d625aa03e9293d00c1fe3736c1b089b12f
13621160019@163.com <13621160019@163.com>
2021-09-20 22:17:25 +0800

    added expiration datetimes to user table in database

--- database/upgrade/upgrade1.2.4.sql
+++ database/upgrade/upgrade1.2.4.sql
@@ -14,5 +14,9 @@ CREATE TABLE IF NOT EXISTS `myems_fdd_db`.`tbl_aliyun_sms_api` (
 
 DROP TABLE IF EXISTS `myems_fdd_db`.`tbl_gsm_modems` ;
 
+
+ALTER TABLE `myems_user_db`.`tbl_users` ADD `password_expiration_datetime_utc` DATETIME NOT NULL DEFAULT '2099-12-31 16:00:00' AFTER privilege_id;
+ALTER TABLE `myems_user_db`.`tbl_users` ADD `account_expiration_datetime_utc` DATETIME NOT NULL DEFAULT '2099-12-31 16:00:00' AFTER privilege_id;
+
 -- UPDATE VERSION NUMBER
 UPDATE myems_system_db.tbl_versions SET version='1.2.4', release_date='2021-09-11' WHERE id=1;
