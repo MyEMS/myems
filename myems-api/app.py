5cd0363acddb6d603b282dccef4dd8786001d34f
Caozhenhui <823914102@qq.com>
2021-11-08 18:13:15 +0800

    CORS Method not found when submit a PUT request

--- myems-api/app.py
+++ myems-api/app.py
@@ -1,3 +1,4 @@
+from falcon_cors import CORS
 import falcon
 from falcon_multipart.middleware import MultipartMiddleware
 from core import energyflowdiagram, privilege, textmessage, distributioncircuit, virtualmeter, \
@@ -90,8 +91,11 @@ from reports import virtualmetercost
 
 # https://github.com/lwcolton/falcon-cors
 # https://github.com/yohanboniface/falcon-multipart
-
-api = falcon.App(middleware=[falcon.CORSMiddleware(), MultipartMiddleware()])
+cors = CORS(allow_all_origins=True,
+            allow_credentials_all_origins=True,
+            allow_all_headers=True,
+            allow_all_methods=True)
+api = falcon.App(middleware=[falcon.CORSMiddleware(allow_origins='*', expose_headers=['Allow']), MultipartMiddleware()])
 
 
 ########################################################################################################################
