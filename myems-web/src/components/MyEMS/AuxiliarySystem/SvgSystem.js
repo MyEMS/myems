ca9f6cee961a8b8924fd38344850e72943d6377a
hyh123a <hyh_youxiang@163.com>
2022-04-21 16:19:46 +0800

    add the svg page

--- myems-web/src/components/MyEMS/AuxiliarySystem/SvgSystem.js
+++ myems-web/src/components/MyEMS/AuxiliarySystem/SvgSystem.js
@@ -10,7 +10,7 @@ import {
   FormGroup,
   Label,
   Row,
-  Spinner,
+  Spinner
 } from 'reactstrap';
 import RealtimeChart from './RealtimeChart';
 import { getCookieValue, createCookie } from '../../../helpers/utils';
@@ -19,10 +19,9 @@ import { withTranslation } from 'react-i18next';
 import uuid from 'uuid/v1';
 import { toast } from 'react-toastify';
 import { APIBaseURL } from '../../../config';
-
+import ButtonIcon from '../../common/ButtonIcon';
 
 const SvgSystem = ({ setRedirect, setRedirectUrl, t }) => {
-
   useEffect(() => {
     let is_logged_in = getCookieValue('is_logged_in');
     let user_name = getCookieValue('user_name');
@@ -42,94 +41,143 @@ const SvgSystem = ({ setRedirect, setRedirectUrl, t }) => {
     }
   });
   let table = createRef();
-  // State
+  // Svg List
+  const [svgSystemList, setSvgSystemList] = useState([]);
+
+  // Svg Content
+  const [svgSystemContent, setSvgSystemContent] = useState(undefined);
+  const [svgSystemContentDict, setSvgSystemContentDict] = useState(undefined);
+
   // Query Parameters
-  const [distributionSystemList, setSvgSystemList] = useState([]);
-  const [selectedSvgSystemName, setSelectedSvgSystemName] = useState(undefined);
   const [selectedSvgSystemID, setSelectedSvgSystemID] = useState(undefined);
-  
+
   //Results
   const [images, setImages] = useState([]);
   const [spinnerHidden, setSpinnerHidden] = useState(false);
 
   useEffect(() => {
     let isResponseOK = false;
-    fetch(APIBaseURL + '/distributionsystems', {
+    fetch(APIBaseURL + '/svgs', {
       method: 'GET',
       headers: {
-        "Content-type": "application/json",
-        "User-UUID": getCookieValue('user_uuid'),
-        "Token": getCookieValue('token')
+        'Content-type': 'application/json',
+        'User-UUID': getCookieValue('user_uuid'),
+        Token: getCookieValue('token')
       },
-      body: null,
-
-    }).then(response => {
-      console.log(response);
-      if (response.ok) {
-        isResponseOK = true;
-      }
-      return response.json();
-    }).then(json => {
-      console.log(json);
-      if (isResponseOK) {
-        // rename keys 
-        json = JSON.parse(JSON.stringify(json).split('"id":').join('"value":').split('"name":').join('"label":'));
-        
+      body: null
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          isResponseOK = true;
+        }
+        return response.json();
+      })
+      .then(json => {
         console.log(json);
-        setSvgSystemList(json);
-        setSelectedSvgSystemID([json[0]].map(o => o.value));
-        setSelectedSvgSystemName([json[0]].map(o => o.label));
-        
-        let images = {};
-        json.forEach((currentValue, index) => {
-          images[currentValue['value']] = {__html: currentValue['svg']}
-        });
-        setImages(images);
-        setSpinnerHidden(true);
-      } else {
-        toast.error(json.description);
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-
+        if (isResponseOK) {
+          console.log('svgs', json);
+          setSvgSystemList(json);
+          setSelectedSvgSystemID(json[0].id);
+          getSvgContent(json[0].id);
+        } else {
+          toast.error(json.description);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
 
   const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
 
-  let onSvgSystemChange = (event) => {
+  let onSvgSystemChange = event => {
     setSelectedSvgSystemID(event.target.value);
-    distributionSystemList.forEach((currentItem, index) => {
-      if (currentItem['value'] === event.target.value) {
-        setSelectedSvgSystemName(currentItem['label']);
-      }
-    });
-    
-  }
+    console.log('onSvgSystemChange ID', event.target.value);
+    // Get Svg Content
+    getSvgContent(event.target.value);
 
+  };
+
+  const getSvgContent = svgId => {
+    let isResponseOK = false;
+    fetch(APIBaseURL + '/svgs/' + svgId, {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        'User-UUID': getCookieValue('user_uuid'),
+        Token: getCookieValue('token')
+      },
+      body: null
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          isResponseOK = true;
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        if (isResponseOK) {
+          console.log('getSvgContent', json.content);
+          setSvgSystemContent(json.content);
+          setSvgSystemContentDict({__html: json.content});
+        } else {
+          toast.error(json.description);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  const viewSvg = () => {
+    getSvgContent(selectedSvgSystemID);
+  };
+
+  useEffect(() => {
+    console.log('svgSystemList', svgSystemList);
+  }, [svgSystemList]);
+
+  useEffect(() => {
+    console.log('svgSystemContent', svgSystemContent);
+  }, [svgSystemContent]);
+
+  useEffect(() => {
+    console.log('selectedSvgSystemID', selectedSvgSystemID);
+  }, [selectedSvgSystemID]);
+
+  useEffect(() => {
+    console.log('svgSystemContentDict', svgSystemContentDict);
+  }, [svgSystemContentDict]);
 
   return (
     <Fragment>
       <div>
         <Breadcrumb>
-          <BreadcrumbItem>{t('Auxiliary System')}</BreadcrumbItem><BreadcrumbItem active>{t('Distribution System')}</BreadcrumbItem>
+          <BreadcrumbItem>{t('Auxiliary System')}</BreadcrumbItem>
+          <BreadcrumbItem active>{t('Svg System')}</BreadcrumbItem>
         </Breadcrumb>
       </div>
       <Card className="bg-light mb-3">
         <CardBody className="p-3">
-          <Form >
+          <Form>
             <Row form>
               <Col xs={6} sm={3}>
                 <FormGroup>
-                  <Label className={labelClasses} for="distributionSystemSelect">
-                    {t('Distribution System')}
+                  <Label className={labelClasses} for="svgSystemSelect">
+                    {t('Svg System')}
                   </Label>
-                  <CustomInput type="select" id="distributionSystemSelect" name="distributionSystemSelect"
-                    value={selectedSvgSystemID} onChange={onSvgSystemChange}
+                  <CustomInput
+                    type="select"
+                    id="svgSystemSelect"
+                    name="svgSystemSelect"
+                    value={selectedSvgSystemID}
+                    onChange={onSvgSystemChange}
                   >
-                    {distributionSystemList.map((distributionSystem, index) => (
-                      <option value={distributionSystem.value} key={distributionSystem.value}>
-                        {distributionSystem.label}
+                    {svgSystemList.map((svgSystem, index) => (
+                      <option value={svgSystem.id} key={svgSystem.id}>
+                        {svgSystem.name}
                       </option>
                     ))}
                   </CustomInput>
@@ -137,27 +185,24 @@ const SvgSystem = ({ setRedirect, setRedirectUrl, t }) => {
               </Col>
               <Col xs="auto">
                 <FormGroup>
-                  <br></br>
-                  <Spinner color="primary" hidden={spinnerHidden}  />
+                  <br />
+                  <Spinner color="primary" hidden={spinnerHidden} />
                 </FormGroup>
-              </Col>              
+              </Col>
             </Row>
+            <Col xs="auto">
+              <br />
+              <ButtonIcon icon="external-link-alt" transform="shrink-3 down-2" color="falcon-default" onClick={viewSvg}>
+                {t('View')}
+              </ButtonIcon>
+            </Col>
           </Form>
         </CardBody>
       </Card>
       <Row noGutters>
-        
-        <Col lg="4" className="pr-lg-2" key={uuid()}>
-          <RealtimeChart 
-            distributionSystemID={selectedSvgSystemID} 
-            distributionSystemName={selectedSvgSystemName}  
-          />
+        <Col lg="10" className="pr-lg-2">
+          <div dangerouslySetInnerHTML={svgSystemContentDict} />
         </Col>
-        
-        <Col lg="8" className="pr-lg-2">
-          <div dangerouslySetInnerHTML={images[selectedSvgSystemID]} />
-        </Col>
-
       </Row>
     </Fragment>
   );
