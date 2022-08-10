e2bdd25eb31820ad9ee1369f09181ce33f179a63
kkomia <583319144@qq.com>
2022-08-10 08:58:25 +0800

    fixed logic issues

--- myems-admin/app/controllers/settings/meter/meter.controller.js
+++ myems-admin/app/controllers/settings/meter/meter.controller.js
@@ -304,9 +304,10 @@ app.controller('ModalAddMeterCtrl', function($scope, $uibModalInstance, params) 
 		var i = 0;
 		var j = 0;
 		$scope.energyitems = [];
-		for (; i < params.energyitems.length; i++, j++){
+		for (; i < params.energyitems.length; i++){
 			if (params.energyitems[i]['energy_category']['id'] == selected_energy_category_id){
 				$scope.energyitems[j] = params.energyitems[i]
+				j = j + 1
 			}
 		}
 	}
