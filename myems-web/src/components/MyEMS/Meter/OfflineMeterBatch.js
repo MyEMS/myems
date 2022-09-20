9c521f5fce1106200ac3968ed4a419ee9f9d2945
caozhenhui <823914102@qq.com>
2022-09-20 13:51:33 +0800

    modified the format of dateRangePicker calendar title of

--- myems-web/src/components/MyEMS/Meter/OfflineMeterBatch.js
+++ myems-web/src/components/MyEMS/Meter/OfflineMeterBatch.js
@@ -71,7 +71,7 @@ const OfflineMeterBatch = ({ setRedirect, setRedirectUrl, t }) => {
     minutes: t('minutes'),
     seconds: t('seconds'),
     last7Days: t('last7Days'),
-    formattedDayPattern: 'yyyy-MM-dd'
+    formattedMonthPattern: 'yyyy-MM-dd'
   };
   const dateRangePickerStyle = { display: 'block', zIndex: 10};
   const { language } = useContext(AppContext);
