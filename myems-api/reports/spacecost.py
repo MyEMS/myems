d10a9260bcc338906f140bd9983f05415813db59
YangZhang-GitHub <2533471770@qq.com>
2022-11-24 10:57:44 +0800

    Base period trend data has been added to the space cost

--- myems-api/reports/spacecost.py
+++ myems-api/reports/spacecost.py
@@ -42,6 +42,7 @@ class Reporting:
         print(req.params)
         space_id = req.params.get('spaceid')
         space_uuid = req.params.get('spaceuuid')
+        comparison_type = req.params.get('comparisontype')
         period_type = req.params.get('periodtype')
         base_period_start_datetime_local = req.params.get('baseperiodstartdatetime')
         base_period_end_datetime_local = req.params.get('baseperiodenddatetime')
@@ -595,6 +596,7 @@ class Reporting:
         result['reporting_period']['units'] = list()
         result['reporting_period']['timestamps'] = list()
         result['reporting_period']['values'] = list()
+        result['reporting_period']['rates'] = list()
         result['reporting_period']['subtotals'] = list()
         result['reporting_period']['subtotals_per_unit_area'] = list()
         result['reporting_period']['toppeaks'] = list()
@@ -626,6 +628,16 @@ class Reporting:
                     base[energy_category_id]['subtotal']
                     if base[energy_category_id]['subtotal'] > 0.0 else None)
                 result['reporting_period']['total'] += reporting[energy_category_id]['subtotal']
+
+                rate = list()
+                for index, value in enumerate(reporting[energy_category_id]['values']):
+                    if index < len(base[energy_category_id]['values']) \
+                            and base[energy_category_id]['values'][index] != 0 and value != 0:
+                        rate.append((value - base[energy_category_id]['values'][index])
+                                    / base[energy_category_id]['values'][index])
+                    else:
+                        rate.append(None)
+                result['reporting_period']['rates'].append(rate)
 
         result['reporting_period']['total_per_unit_area'] = \
             result['reporting_period']['total'] / space['area'] if space['area'] > 0.0 else None
@@ -661,8 +673,11 @@ class Reporting:
         if not is_quick_mode:
             result['excel_bytes_base64'] = excelexporters.spacecost.export(result,
                                                                            space['name'],
+                                                                           base_period_start_datetime_local,
+                                                                           base_period_end_datetime_local,
                                                                            reporting_period_start_datetime_local,
                                                                            reporting_period_end_datetime_local,
                                                                            period_type,
-                                                                           language)
+                                                                           language,
+                                                                           comparison_type)
         resp.text = json.dumps(result)
