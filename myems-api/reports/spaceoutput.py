a82eb5a22b8438687ade77361dfaa2ab85da4de6
YangZhang-GitHub <2533471770@qq.com>
2022-11-28 13:57:13 +0800

    Base period trend data has been added to the space output

--- myems-api/reports/spaceoutput.py
+++ myems-api/reports/spaceoutput.py
@@ -42,6 +42,7 @@ class Reporting:
         print(req.params)
         space_id = req.params.get('spaceid')
         space_uuid = req.params.get('spaceuuid')
+        comparison_type = req.params.get('comparisontype')
         period_type = req.params.get('periodtype')
         base_period_start_datetime_local = req.params.get('baseperiodstartdatetime')
         base_period_end_datetime_local = req.params.get('baseperiodenddatetime')
@@ -575,6 +576,7 @@ class Reporting:
         result['reporting_period']['units'] = list()
         result['reporting_period']['timestamps'] = list()
         result['reporting_period']['values'] = list()
+        result['reporting_period']['rates'] = list()
         result['reporting_period']['subtotals'] = list()
         result['reporting_period']['subtotals_per_unit_area'] = list()
         result['reporting_period']['increment_rates'] = list()
@@ -593,6 +595,16 @@ class Reporting:
                     (reporting[energy_category_id]['subtotal'] - base[energy_category_id]['subtotal']) /
                     base[energy_category_id]['subtotal']
                     if base[energy_category_id]['subtotal'] > 0.0 else None)
+
+                rate = list()
+                for index, value in enumerate(reporting[energy_category_id]['values']):
+                    if index < len(base[energy_category_id]['values']) \
+                            and base[energy_category_id]['values'][index] != 0 and value != 0:
+                        rate.append((value - base[energy_category_id]['values'][index])
+                                    / base[energy_category_id]['values'][index])
+                    else:
+                        rate.append(None)
+                result['reporting_period']['rates'].append(rate)
 
         result['parameters'] = {
             "names": parameters_data['names'],
@@ -618,9 +630,12 @@ class Reporting:
         if not is_quick_mode:
             result['excel_bytes_base64'] = excelexporters.spaceoutput.export(result,
                                                                              space['name'],
+                                                                             base_period_start_datetime_local,
+                                                                             base_period_end_datetime_local,
                                                                              reporting_period_start_datetime_local,
                                                                              reporting_period_end_datetime_local,
                                                                              period_type,
-                                                                             language)
+                                                                             language,
+                                                                             comparison_type)
 
         resp.text = json.dumps(result)
