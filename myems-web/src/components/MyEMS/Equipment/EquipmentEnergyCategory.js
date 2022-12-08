e95aefe02e44e33ef5328e17df200d43d7372fda
YangZhang-GitHub <2533471770@qq.com>
2022-12-08 08:59:00 +0800

    Delete useless imports

--- myems-web/src/components/MyEMS/Equipment/EquipmentEnergyCategory.js
+++ myems-web/src/components/MyEMS/Equipment/EquipmentEnergyCategory.js
@@ -21,7 +21,6 @@ import loadable from '@loadable/component';
 import Cascader from 'rc-cascader';
 import CardSummary from '../common/CardSummary';
 import MultiTrendChart from '../common/MultiTrendChart';
-import LineChart from '../common/LineChart';
 import SharePie from '../common/SharePie';
 import { getCookieValue, createCookie } from '../../../helpers/utils';
 import withRedirect from '../../../hoc/withRedirect';
