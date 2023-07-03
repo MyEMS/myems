94d97447d30e049e401cc5c3846fe0a9a90b93a6
nengyuanzhang <13011132526@163.com>
2023-07-03 11:56:06 +0800

    added access control to Sensor in myems-admin

--- myems-admin/app/services/settings/sensor/sensor.service.js
+++ myems-admin/app/services/settings/sensor/sensor.service.js
@@ -1,16 +1,16 @@
 'use strict';
 app.factory('SensorService', function($http) {
     return {
-        getAllSensors:function(callback){
-            $http.get(getAPI()+'sensors')
+        getAllSensors:function(headers, callback){
+            $http.get(getAPI()+'sensors', {headers})
             .then(function (response) {
                 callback(response);
             }, function (response) {
                 callback(response);
             });
         },
-        searchSensors: function(query, callback) {
-            $http.get(getAPI()+'sensors', { params: { q: query } })
+        searchSensors: function(query, headers, callback) {
+            $http.get(getAPI()+'sensors', { params: { q: query } }, {headers})
             .then(function (response) {
                 callback(response);
             }, function (response) {
