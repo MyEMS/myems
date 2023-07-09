4763361210721b9980f849f038c61942c7757691
nengyuanzhang <13011132526@163.com>
2023-07-09 22:26:47 +0800

    added related parameters to microgrid report

--- myems-web/src/components/MyEMS/Microgrid/Microgrid.js
+++ myems-web/src/components/MyEMS/Microgrid/Microgrid.js
@@ -12,6 +12,7 @@ import {
 } from 'reactstrap';
 import CardSummary from '../common/CardSummary';
 import CountUp from 'react-countup';
+import MultipleLineChart from '../common/MultipleLineChart';
 import LineChart from '../common/LineChart';
 import BarChart from '../common/BarChart';
 import ChartSpacesStackBar from '../common/ChartSpacesStackBar';
@@ -26,6 +27,99 @@ import { Map } from 'react-leaflet';
 
 
 const Microgrid = ({ setRedirect, setRedirectUrl, t }) => {
+
+  useEffect(() => {
+    let is_logged_in = getCookieValue('is_logged_in');
+    let user_name = getCookieValue('user_name');
+    let user_display_name = getCookieValue('user_display_name');
+    let user_uuid = getCookieValue('user_uuid');
+    let token = getCookieValue('token');
+    if (is_logged_in === null || !is_logged_in) {
+      setRedirectUrl(`/authentication/basic/login`);
+      setRedirect(true);
+    } else {
+      //update expires time of cookies
+      createCookie('is_logged_in', true, 1000 * 60 * 10 * 1);
+      createCookie('user_name', user_name, 1000 * 60 * 10 * 1);
+      createCookie('user_display_name', user_display_name, 1000 * 60 * 10 * 1);
+      createCookie('user_uuid', user_uuid, 1000 * 60 * 10 * 1);
+      createCookie('token', token, 1000 * 60 * 10 * 1);
+    }
+  });
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      let is_logged_in = getCookieValue('is_logged_in');
+      if (is_logged_in === null || !is_logged_in) {
+        setRedirectUrl(`/authentication/basic/login`);
+        setRedirect(true);
+      }
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+
+  // State
+  // Query Parameters
+  const [microgridList, setMicrogridList] = useState([]);
+  const [selectedMicrogridID, setSelectedMicrogridID] = useState(undefined);
+
+  //Results
+  const [images, setImages] = useState([]);
+  const [spinnerHidden, setSpinnerHidden] = useState(false);
+
+  const [parameterLineChartLabels, setParameterLineChartLabels] = useState([]);
+  const [parameterLineChartData, setParameterLineChartData] = useState({});
+  const [parameterLineChartOptions, setParameterLineChartOptions] = useState([]);
+
+  useEffect(() => {
+    let isResponseOK = false;
+    fetch(APIBaseURL + '/microgrids', {
+      method: 'GET',
+      headers: {
+        "Content-type": "application/json",
+        "User-UUID": getCookieValue('user_uuid'),
+        "Token": getCookieValue('token')
+      },
+      body: null,
+
+    }).then(response => {
+      console.log(response);
+      if (response.ok) {
+        isResponseOK = true;
+      }
+      return response.json();
+    }).then(json => {
+      console.log(json);
+      if (isResponseOK) {
+        // rename keys
+        json = JSON.parse(JSON.stringify(json).split('"id":').join('"value":').split('"name":').join('"label":'));
+
+        console.log(json);
+        setMicrogridList(json);
+        setSelectedMicrogridID([json[0]].map(o => o.value));
+        refreshMicrogridReport([json[0]].map(o => o.value));
+        let images = {};
+        json.forEach((currentValue, index) => {
+          images[currentValue['value']] = {__html: currentValue['svg']}
+        });
+        setImages(images);
+        setSpinnerHidden(true);
+      } else {
+        toast.error(t(json.description));
+      }
+    }).catch(err => {
+      console.log(err);
+    });
+
+  }, []);
+
+  const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
+
+  let onMicrogridChange = (event) => {
+    setSelectedMicrogridID(event.target.value);
+    refreshMicrogridReport(event.target.value);
+  };
 
   const refreshSVGData =()=> {
     let isResponseOK = false;
@@ -58,50 +152,16 @@ const Microgrid = ({ setRedirect, setRedirectUrl, t }) => {
     .catch(err => {
       console.log(err);
     });
-  }
+  };
 
-  useEffect(() => {
-    let is_logged_in = getCookieValue('is_logged_in');
-    let user_name = getCookieValue('user_name');
-    let user_display_name = getCookieValue('user_display_name');
-    let user_uuid = getCookieValue('user_uuid');
-    let token = getCookieValue('token');
-    if (is_logged_in === null || !is_logged_in) {
-      setRedirectUrl(`/authentication/basic/login`);
-      setRedirect(true);
-    } else {
-      //update expires time of cookies
-      createCookie('is_logged_in', true, 1000 * 60 * 10 * 1);
-      createCookie('user_name', user_name, 1000 * 60 * 10 * 1);
-      createCookie('user_display_name', user_display_name, 1000 * 60 * 10 * 1);
-      createCookie('user_uuid', user_uuid, 1000 * 60 * 10 * 1);
-      createCookie('token', token, 1000 * 60 * 10 * 1);
-    }
-  });
+  useInterval(() => {
+    refreshSVGData();
+  }, 1000 * 10);
 
-  useEffect(() => {
-    let timer = setInterval(() => {
-      let is_logged_in = getCookieValue('is_logged_in');
-      if (is_logged_in === null || !is_logged_in) {
-        setRedirectUrl(`/authentication/basic/login`);
-        setRedirect(true);
-      }
-    }, 1000);
-    return () => clearInterval(timer);
-  }, []);
 
-  // State
-  // Query Parameters
-  const [microgridList, setMicrogridList] = useState([]);
-  const [selectedMicrogridID, setSelectedMicrogridID] = useState(undefined);
-
-  //Results
-  const [images, setImages] = useState([]);
-  const [spinnerHidden, setSpinnerHidden] = useState(false);
-
-  useEffect(() => {
+  const refreshMicrogridReport =(microgridID)=> {
     let isResponseOK = false;
-    fetch(APIBaseURL + '/microgrids', {
+    fetch(APIBaseURL + '/reports/microgrid?microgridid=' + microgridID, {
       method: 'GET',
       headers: {
         "Content-type": "application/json",
@@ -111,46 +171,37 @@ const Microgrid = ({ setRedirect, setRedirectUrl, t }) => {
       body: null,
 
     }).then(response => {
-      console.log(response);
       if (response.ok) {
         isResponseOK = true;
       }
       return response.json();
     }).then(json => {
-      console.log(json);
       if (isResponseOK) {
-        // rename keys
-        json = JSON.parse(JSON.stringify(json).split('"id":').join('"value":').split('"name":').join('"label":'));
-
         console.log(json);
-        setMicrogridList(json);
-        setSelectedMicrogridID([json[0]].map(o => o.value));
-
-        let images = {};
-        json.forEach((currentValue, index) => {
-          images[currentValue['value']] = {__html: currentValue['svg']}
+        let timestamps = {}
+        json['parameters']['timestamps'].forEach((currentValue, index) => {
+          timestamps['a' + index] = currentValue;
         });
-        setImages(images);
-        setSpinnerHidden(true);
-      } else {
-        toast.error(t(json.description));
+        setParameterLineChartLabels(timestamps);
+
+        let values = {}
+        json['parameters']['values'].forEach((currentValue, index) => {
+          values['a' + index] = currentValue;
+        });
+        setParameterLineChartData(values);
+
+        let names = Array();
+        json['parameters']['names'].forEach((currentValue, index) => {
+
+          names.push({ 'value': 'a' + index, 'label': currentValue });
+        });
+        setParameterLineChartOptions(names);
       }
-    }).catch(err => {
+    })
+    .catch(err => {
       console.log(err);
     });
-
-  }, []);
-
-  const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
-
-  let onMicrogridChange = (event) => {
-    setSelectedMicrogridID(event.target.value);
   };
-
-
-  useInterval(() => {
-    refreshSVGData();
-  }, 1000 * 3);
 
   return (
     <Fragment>
@@ -211,7 +262,12 @@ const Microgrid = ({ setRedirect, setRedirectUrl, t }) => {
         </Col>
 
         <Col lg="6" className="pr-lg-2">
-
+          <MultipleLineChart reportingTitle={t('Related Parameters')}
+            baseTitle=''
+            labels={parameterLineChartLabels}
+            data={parameterLineChartData}
+            options={parameterLineChartOptions}>
+          </MultipleLineChart>
         </Col>
 
       </Row>
