d9a4376dd73b77e89a4313745c08e5e6180a24a5
caozhenhui <823914102@qq.com>
2023-07-14 09:37:40 +0800

    modified the map

--- myems-web/src/config.js
+++ myems-web/src/config.js
@@ -1,8 +1,8 @@
-export const version = '3.6.0';
+export const version = '3.7.0';
 export const navbarBreakPoint = 'xl'; // Vertical navbar breakpoint
 export const topNavbarBreakpoint = 'lg';
-export const APIBaseURL = 'http://127.0.0.1:8000';
-//export const APIBaseURL = window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api";
+//export const APIBaseURL = 'http://127.0.0.1:8000';
+export const APIBaseURL = window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api";
 export const settings = {
   isFluid: true,
   isRTL: false,
@@ -16,8 +16,6 @@ export const settings = {
   currency: '$',
   isNavbarVerticalCollapsed: false,
   navbarStyle: 'transparent',
-  language: 'zh_CN', //en, de, zh_CN
-  latitude: 39.5247,
-  longitude: 116.2317,
+  language: 'zh_CN' //en, de, zh_CN
 };
 export default { version, navbarBreakPoint, topNavbarBreakpoint, settings, APIBaseURL };
