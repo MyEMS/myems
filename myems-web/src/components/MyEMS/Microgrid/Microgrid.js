5eca8fdf241c349a98b16f55872b70a14efd0f05
caozhenhui <823914102@qq.com>
2023-07-25 14:33:59 +0800

    modified the svg animation

--- myems-web/src/components/MyEMS/Microgrid/Microgrid.js
+++ myems-web/src/components/MyEMS/Microgrid/Microgrid.js
@@ -25,7 +25,6 @@ import { toast } from 'react-toastify';
 import { APIBaseURL } from '../../../config';
 import useInterval from '../../../hooks/useInterval';
 import { Map } from 'react-leaflet';
-import '../../../assets/img/illustrations/diagram1.svg'
 
 
 const Microgrid = ({ setRedirect, setRedirectUrl, t }) => {
