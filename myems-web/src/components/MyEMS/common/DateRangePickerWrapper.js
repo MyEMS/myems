5f54dbf30f1f3d156c3089a9377948ce2a350c46
caozhenhui <823914102@qq.com>
2023-09-06 16:26:28 +0800

    modified the daterangepicker

--- myems-web/src/components/MyEMS/common/DateRangePickerWrapper.js
+++ myems-web/src/components/MyEMS/common/DateRangePickerWrapper.js
@@ -13,100 +13,12 @@ const DateRangePickerWrapper = ({id, disabled, format, value, onChange, size, st
         let calendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1];
         let calendarTitleObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[0];
         if(flag) {
-          calendarObj.children[0].children[0].children[0].children[1].innerText = time;
           setTimeout(() => {
             calendarTitleObj.childNodes[0].nodeValue = time + ' ' + calendarObj.children[0].children[0].children[1].innerText;
           }, 0);
-        }else{
-          if (moment(calendarObj.children[0].children[0].children[0].children[1].innerText).isBefore(time)){
-            calendarObj.children[1].children[0].children[0].children[1].innerText = time;
-          } else {
-            calendarObj.children[1].children[0].children[0].children[1].innerText =
-             calendarObj.children[0].children[0].children[0].children[1].innerText;
-            calendarObj.children[0].children[0].children[0].children[1].innerText = time
-          }
         }
         flag = !flag;
       }
-
-    const onOpen = () => {
-      flag = true;
-      let calendarTitleObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[0];
-      let calendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1];
-      calendarObj.children[0].children[0].children[0].children[1].innerText = calendarTitleObj.firstChild.data.split(' ')[0];
-      calendarObj.children[1].children[0].children[0].children[1].innerText = calendarTitleObj.lastChild.data.split(' ')[0];
-      calendarTitleObj.childNodes[0].nodeValue = placeholder;
-      if (calendarTitleObj.childNodes[1]) {
-        calendarTitleObj.childNodes[1].nodeValue = '';
-      }
-      if (calendarTitleObj.childNodes[2]) {
-        calendarTitleObj.childNodes[2].nodeValue = '';
-      }
-    }
-
-    const onEntered = () => {
-      let leftCalendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1].children[0];
-      let rightCalendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1].children[1];
-      let leftCalendarObjLeftAngle = leftCalendarObj.children[0].children[0].children[0];
-      let leftCalendarObjRightAngle = leftCalendarObj.children[0].children[0].children[2];
-      let rightCalendarObjLeftAngle = rightCalendarObj.children[0].children[0].children[0];
-      let rightCalendarObjRightAngle = rightCalendarObj.children[0].children[0].children[2];
-      prevMonth(leftCalendarObjLeftAngle, false, leftCalendarObj.children[0].children[0].children[1], rightCalendarObj.children[0].children[0].children[1]);
-      nextMonth(leftCalendarObjRightAngle, true, leftCalendarObj.children[0].children[0].children[1], rightCalendarObj.children[0].children[0].children[1]);
-      prevMonth(rightCalendarObjLeftAngle, true, leftCalendarObj.children[0].children[0].children[1], rightCalendarObj.children[0].children[0].children[1]);
-      nextMonth(rightCalendarObjRightAngle, false, leftCalendarObj.children[0].children[0].children[1], rightCalendarObj.children[0].children[0].children[1]);
-    }
-
-    const onExited = () => {
-      let leftCalendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1].children[0];
-      let rightCalendarObj = Ref.current.overlay.children[0].children[0].children[0].children[0].children[1].children[1];
-      leftCalendarObj.children[0].children[0].children[0].onclick = null;
-      leftCalendarObj.children[0].children[0].children[2].onclick = null;
-      rightCalendarObj.children[0].children[0].children[0].onclick = null;
-      rightCalendarObj.children[0].children[0].children[2].onclick = null;
-    }
-
-    const prevMonth = (angleObj, flag, startMonth, endMonth) => {
-      if (angleObj.onclick.length == 0){
-        if (flag) {
-          angleObj.onclick = () => {
-            let prevMonth = moment(endMonth.innerText);
-            if (prevMonth.subtract(1,'months').isSameOrBefore(startMonth.innerText, 'month')) {
-              endMonth.innerText = prevMonth.startOf('month').format('YYYY-MM-DD');
-              startMonth.innerText = moment(endMonth.innerText).subtract(1,'months').startOf('month').format('YYYY-MM-DD');
-            } else {
-              endMonth.innerText = prevMonth.startOf('month').format('YYYY-MM-DD');
-            }
-          }
-        } else {
-          angleObj.onclick = () => {
-            let prevMonth = moment(startMonth.innerText);
-            startMonth.innerText = prevMonth.subtract(1, "months").startOf('month').format('YYYY-MM-DD');
-          }
-        }
-      }
-    }
-
-    const nextMonth = (angleObj, flag, startMonth, endMonth) => {
-      if (angleObj.onclick.length == 0){
-        if (flag) {
-          angleObj.onclick = () => {
-            let nextMonth= moment(startMonth.innerText);
-            if (nextMonth.add(1,'months').isSameOrAfter(endMonth.innerText, 'month')) {
-              startMonth.innerText = nextMonth.startOf('month').format('YYYY-MM-DD');
-              endMonth.innerText = moment(startMonth.innerText).add(1,'months').startOf('month').format('YYYY-MM-DD');
-            } else {
-              startMonth.innerText = nextMonth.startOf('month').format('YYYY-MM-DD');
-            }
-          }
-        } else {
-          angleObj.onclick = () => {
-            let nextMonth= moment(endMonth.innerText);
-            endMonth.innerText = nextMonth.add(1, "months").startOf('month').format('YYYY-MM-DD');
-          }
-        }
-      }
-    }
 
     return (
         <DateRangePicker
@@ -122,9 +34,6 @@ const DateRangePickerWrapper = ({id, disabled, format, value, onChange, size, st
             locale={locale}
             placeholder={placeholder}
             onSelect={onSelected}
-            onOpen={onOpen}
-            onEntered={onEntered}
-            onExited={onExited}
             ref={Ref}
         />
     )
