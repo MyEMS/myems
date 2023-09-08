4495c8dca7f704196f7b99c120732727146c218c
caozhenhui <823914102@qq.com>
2023-09-08 15:12:57 +0800

    added Thai translation

--- myems-web/src/components/MyEMS/auth/LoginForm.js
+++ myems-web/src/components/MyEMS/auth/LoginForm.js
@@ -182,6 +182,7 @@ const LoginForm = ({ setRedirect, hasLabel, layout, t }) => {
         <option value="ru">{t('language-ru')}</option>
         <option value="ar">{t('language-ar')}</option>
         <option value="vi">{t('language-vi')}</option>
+        <option value="th">{t('language-th')}</option>
       </CustomInput>
       <Row className="justify-content-center align-items-center">
         <Col xs="auto">
