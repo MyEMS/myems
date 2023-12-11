494ee85bbde5a536e86e18d1d5dcc1c50dd905d4
nengyuanzhang <13011132526@163.com>
2023-12-11 21:03:25 +0800

    updated root space initial value in database

--- database/install/myems_system_db.sql
+++ database/install/myems_system_db.sql
@@ -1104,10 +1104,10 @@ START TRANSACTION;
 USE `myems_system_db`;
 
 INSERT INTO `myems_system_db`.`tbl_spaces`
-(`id`, `name`, `uuid`, `parent_space_id`, `area`, `timezone_id`, `contact_id`, `is_input_counted`, `is_output_counted`, `cost_center_id`, `description`)
+(`id`, `name`, `uuid`, `parent_space_id`, `area`, `timezone_id`, `contact_id`, `is_input_counted`, `is_output_counted`, `cost_center_id`, `latitude`, `longitude`, `description`)
 VALUES
 -- DO NOT deleted the record which ID is 1. It's the root space.
-(1, 'MyEMS Headquarter', '9dfb7cff-f19f-4a1e-8c79-3adf6425bfd9', NULL, 99999.999, 56, 1, true, true, 1,  'MyEMS Project');
+(1, 'MyEMS', '9dfb7cff-f19f-4a1e-8c79-3adf6425bfd9', NULL, 99999.999, 56, 1, true, true, 1, 39.915119, 116.403963, 'MyEMS Space');
 COMMIT;
 
 -- ---------------------------------------------------------------------------------------------------------------------
