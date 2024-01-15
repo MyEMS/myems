3d5a9fa61b324803351380a3890e2ed8eca62380
nengyuanzhang <13011132526@163.com>
2024-01-15 13:27:38 +0800

    added arrow up or arrow down icon to cardsummary in myems-web

--- myems-web/src/components/MyEMS/common/CardSummary.js
+++ myems-web/src/components/MyEMS/common/CardSummary.js
@@ -6,6 +6,7 @@ import Background from '../../common/Background';
 import corner1 from '../../../assets/img/illustrations/corner-1.png';
 import corner2 from '../../../assets/img/illustrations/corner-2.png';
 import corner3 from '../../../assets/img/illustrations/corner-3.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const getImage = color => {
   switch (color) {
@@ -20,6 +21,16 @@ const getImage = color => {
   }
 };
 
+const getIcon = rate => {
+  let r = parseFloat(rate.substring(0,rate.length-1))
+  if (r > 0.0)
+    return <FontAwesomeIcon icon="arrow-up"/>;
+  else if (r < 0.0)
+    return <FontAwesomeIcon icon="arrow-down"/>;
+  else
+    return null;
+};
+
 const getContentClassNames = color => {
   const contentClassNames = 'display-4 fs-4 mb-2 font-weight-normal text-sans-serif';
   if (color === 'success') return contentClassNames;
@@ -27,13 +38,16 @@ const getContentClassNames = color => {
 };
 
 const CardSummary = ({ title, rate, color, children, footnote, footvalue, footunit }) => {
+
+
+
   return (
     <Card className="mb-3 overflow-hidden" style={{ minWidth: '12rem' }}>
       <Background image={getImage(color)} className="bg-card" />
       <CardBody className="position-relative">
         <h6>
           {title}
-          <span className={`badge badge-soft-${color} rounded-capsule ml-2`}>{rate}</span>
+          <span className={`badge badge-soft-${color} rounded-capsule ml-2`}>{getIcon(rate)}{rate}</span>
         </h6>
         <div className={getContentClassNames(color)}>{children}</div>
         <h6 className="font-weight-semi-bold fs--1 text-nowrap">
