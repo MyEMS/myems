67096b6cb7a9fd0fafed3292b280aba5607a7de3
llis <2227043858@qq.com>
2024-01-25 21:30:23 +0800

    Added the Import, Export, and Clone button to the Combined Equipment

--- myems-admin/app/services/settings/combinedequipment/combinedequipment.service.js
+++ myems-admin/app/services/settings/combinedequipment/combinedequipment.service.js
@@ -40,6 +40,30 @@ app.factory('CombinedEquipmentService', function($http) {
             }, function (response) {
                 callback(response);
             });
+        },
+        exportCombinedEquipment: function(combinedequipment, headers, callback) {
+            $http.get(getAPI()+'combinedequipments/'+combinedequipment.id+'/export', {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        importCombinedEquipment: function(importdata, headers, callback) {
+            $http.post(getAPI()+'combinedequipments/import', JSON.parse(importdata), {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        cloneCombinedEquipment: function(combinedequipment, headers, callback) {
+            $http.post(getAPI()+'combinedequipments/'+combinedequipment.id+'/clone', {data:null}, {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
         }
     };
 });
