4c06e4af603f4d6339be474ec7a8d99794502a12
llis <2227043858@qq.com>
2024-02-07 19:18:35 +0800

    Added the Import, Export, and Clone button to the rule, the point

--- myems-admin/app/services/settings/datasource/point.service.js
+++ myems-admin/app/services/settings/datasource/point.service.js
@@ -48,6 +48,30 @@ app.factory('PointService', function($http) {
             }, function (response) {
                 callback(response);
             });  
-        }
+        },
+        exportPoint: function(point, headers, callback) {
+            $http.get(getAPI()+'points/'+point.id+'/export', {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        importPoint: function(importdata, headers, callback) {
+            $http.post(getAPI()+'points/import', JSON.parse(importdata), {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        clonePoint: function(point, headers, callback) {
+            $http.post(getAPI()+'points/'+point.id+'/clone', {data:null}, {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
     };
 });  
