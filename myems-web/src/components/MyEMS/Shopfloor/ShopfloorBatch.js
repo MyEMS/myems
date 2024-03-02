395664ea9334dcc5b5bd80164d8dfa1478a3b4fc
wmiao <1106063169@qq.com>
2024-03-02 20:14:10 +0800

    fixed warnings in src/components/MyEMS/Shopfloor

--- myems-web/src/components/MyEMS/Shopfloor/ShopfloorBatch.js
+++ myems-web/src/components/MyEMS/Shopfloor/ShopfloorBatch.js
@@ -12,7 +12,8 @@ import {
   FormGroup,
   Input,
   Label,
-  Spinner, Media
+  Spinner,
+  Media
 } from 'reactstrap';
 import moment from 'moment';
 import loadable from '@loadable/component';
@@ -25,13 +26,12 @@ import ButtonIcon from '../../common/ButtonIcon';
 import { APIBaseURL, settings } from '../../../config';
 
 import DateRangePickerWrapper from '../common/DateRangePickerWrapper';
-import { endOfDay} from 'date-fns';
+import { endOfDay } from 'date-fns';
 import AppContext from '../../../context/Context';
-import {Link} from "react-router-dom";
-import Flex from "../../common/Flex";
+import { Link } from 'react-router-dom';
+import Flex from '../../common/Flex';
 
 const DetailedDataTable = loadable(() => import('../common/DetailedDataTable'));
-
 
 const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
   let current_moment = moment();
@@ -41,7 +41,7 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
     let user_display_name = getCookieValue('user_display_name');
     let user_uuid = getCookieValue('user_uuid');
     let token = getCookieValue('token');
-    if (checkEmpty(is_logged_in) || checkEmpty(token)|| checkEmpty(user_uuid) || !is_logged_in) {
+    if (checkEmpty(is_logged_in) || checkEmpty(token) || checkEmpty(user_uuid) || !is_logged_in) {
       setRedirectUrl(`/authentication/basic/login`);
       setRedirect(true);
     } else {
@@ -71,7 +71,13 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
   const [selectedSpaceID, setSelectedSpaceID] = useState(undefined);
   const [shopfloorList, setShopfloorList] = useState([]);
   const [cascaderOptions, setCascaderOptions] = useState(undefined);
-  const [reportingPeriodDateRange, setReportingPeriodDateRange] = useState([current_moment.clone().startOf('month').toDate(), current_moment.toDate()]);
+  const [reportingPeriodDateRange, setReportingPeriodDateRange] = useState([
+    current_moment
+      .clone()
+      .startOf('month')
+      .toDate(),
+    current_moment.toDate()
+  ]);
   const dateRangePickerLocale = {
     sunday: t('sunday'),
     monday: t('monday'),
@@ -89,7 +95,7 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
     last7Days: t('last7Days'),
     formattedMonthPattern: 'yyyy-MM-dd'
   };
-  const dateRangePickerStyle = { display: 'block', zIndex: 10};
+  const dateRangePickerStyle = { display: 'block', zIndex: 10 };
   const { language } = useContext(AppContext);
 
   // buttons
@@ -98,8 +104,10 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
   const [exportButtonHidden, setExportButtonHidden] = useState(true);
 
   //Results
-  const [detailedDataTableColumns, setDetailedDataTableColumns] = useState(
-    [{dataField: 'name', text: t('Name'), sort: true}, {dataField: 'space', text: t('Space'), sort: true}]);
+  const [detailedDataTableColumns, setDetailedDataTableColumns] = useState([
+    { dataField: 'name', text: t('Name'), sort: true },
+    { dataField: 'space', text: t('Space'), sort: true }
+  ]);
   const [excelBytesBase64, setExcelBytesBase64] = useState(undefined);
 
   useEffect(() => {
@@ -111,33 +119,40 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
         'User-UUID': getCookieValue('user_uuid'),
         Token: getCookieValue('token')
       },
-      body: null,
+      body: null
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          isResponseOK = true;
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        if (isResponseOK) {
+          // rename keys
+          json = JSON.parse(
+            JSON.stringify([json])
+              .split('"id":')
+              .join('"value":')
+              .split('"name":')
+              .join('"label":')
+          );
+          setCascaderOptions(json);
+          // set the default selected space
+          setSelectedSpaceName([json[0]].map(o => o.label));
+          setSelectedSpaceID([json[0]].map(o => o.value));
 
-    }).then(response => {
-      console.log(response);
-      if (response.ok) {
-        isResponseOK = true;
-      }
-      return response.json();
-    }).then(json => {
-      console.log(json);
-      if (isResponseOK) {
-        // rename keys
-        json = JSON.parse(JSON.stringify([json]).split('"id":').join('"value":').split('"name":').join('"label":'));
-        setCascaderOptions(json);
-        // set the default selected space
-        setSelectedSpaceName([json[0]].map(o => o.label));
-        setSelectedSpaceID([json[0]].map(o => o.value));
-
-        setSubmitButtonDisabled(false);
-        setSpinnerHidden(true);
-      } else {
-        toast.error(t(json.description));
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-
+          setSubmitButtonDisabled(false);
+          setSpinnerHidden(true);
+        } else {
+          toast.error(t(json.description));
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }, []);
 
   const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
@@ -151,8 +166,8 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
   };
 
   // Callback fired when value changed
-  let onReportingPeriodChange = (DateRange) => {
-    if(DateRange == null) {
+  let onReportingPeriodChange = DateRange => {
+    if (DateRange == null) {
       setReportingPeriodDateRange([null, null]);
     } else {
       if (moment(DateRange[1]).format('HH:mm:ss') === '00:00:00') {
@@ -168,13 +183,12 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
     setReportingPeriodDateRange([null, null]);
   };
 
-
   // Handler
   const handleSubmit = e => {
     e.preventDefault();
     console.log('handleSubmit');
     console.log(selectedSpaceID);
-    console.log(moment(reportingPeriodDateRange[0]).format('YYYY-MM-DDTHH:mm:ss'))
+    console.log(moment(reportingPeriodDateRange[0]).format('YYYY-MM-DDTHH:mm:ss'));
     console.log(moment(reportingPeriodDateRange[1]).format('YYYY-MM-DDTHH:mm:ss'));
 
     // disable submit button
@@ -182,114 +196,123 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
     // show spinner
     setSpinnerHidden(false);
     // hide export button
-    setExportButtonHidden(true)
+    setExportButtonHidden(true);
 
     // Reinitialize tables
     setShopfloorList([]);
 
     let isResponseOK = false;
-    fetch(APIBaseURL + '/reports/shopfloorbatch?' +
-      'spaceid=' + selectedSpaceID +
-      '&reportingperiodstartdatetime=' + moment(reportingPeriodDateRange[0]).format('YYYY-MM-DDTHH:mm:ss') +
-      '&reportingperiodenddatetime=' + moment(reportingPeriodDateRange[1]).format('YYYY-MM-DDTHH:mm:ss') +
-      '&language=' + language, {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-        'User-UUID': getCookieValue('user_uuid'),
-        Token: getCookieValue('token')
-      },
-      body: null,
-
-    }).then(response => {
-      if (response.ok) {
-        isResponseOK = true;
-      };
-      return response.json();
-    }).then(json => {
-      if (isResponseOK) {
-        console.log(json);
-        let detailed_column_list = [];
-        detailed_column_list.push({
-          dataField: 'name',
-          text: t('Name'),
-          formatter: nameFormatter,
-          sort: true
-        });
-        detailed_column_list.push({
-          dataField: 'space',
-          text: t('Space'),
-          sort: true
-        });
-        json['energycategories'].forEach((currentValue, index) => {
-          detailed_column_list.push({
-            dataField: 'a' + index,
-            text: currentValue['name'] + ' (' + currentValue['unit_of_measure'] + ')',
-            sort: true,
-            formatter: function (decimalValue) {
-              if (typeof decimalValue === 'number') {
-                return decimalValue.toFixed(2);
-              } else {
-                return null;
-              }
-            }
-          })
-        });
-        setDetailedDataTableColumns(detailed_column_list);
-        let shopfloors = [];
-        if (json['shopfloors'].length > 0) {
-          json['shopfloors'].forEach((currentShopfloor, index) => {
-            let detailed_value = {};
-            detailed_value['id'] = currentShopfloor['id'];
-            detailed_value['name'] = currentShopfloor['shopfloor_name'];
-            detailed_value['uuid'] = currentShopfloor['shopfloor_uuid'];
-            detailed_value['space'] = currentShopfloor['space_name'];
-            detailed_value['costcenter'] = currentShopfloor['cost_center_name'];
-            currentShopfloor['values'].forEach((currentValue, energyCategoryIndex) => {
-              detailed_value['a' + energyCategoryIndex] = currentValue;
-            });
-            shopfloors.push(detailed_value);
-          });
-        };
-
-        setShopfloorList(shopfloors);
-
-        setExcelBytesBase64(json['excel_bytes_base64']);
-
-        // enable submit button
-        setSubmitButtonDisabled(false);
-        // hide spinner
-        setSpinnerHidden(true);
-        // show export button
-        setExportButtonHidden(false);
-
-      } else {
-        toast.error(t(json.description));
+    fetch(
+      APIBaseURL +
+        '/reports/shopfloorbatch?' +
+        'spaceid=' +
+        selectedSpaceID +
+        '&reportingperiodstartdatetime=' +
+        moment(reportingPeriodDateRange[0]).format('YYYY-MM-DDTHH:mm:ss') +
+        '&reportingperiodenddatetime=' +
+        moment(reportingPeriodDateRange[1]).format('YYYY-MM-DDTHH:mm:ss') +
+        '&language=' +
+        language,
+      {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+          'User-UUID': getCookieValue('user_uuid'),
+          Token: getCookieValue('token')
+        },
+        body: null
       }
-    }).catch(err => {
-      console.log(err);
-    });
+    )
+      .then(response => {
+        if (response.ok) {
+          isResponseOK = true;
+        }
+        return response.json();
+      })
+      .then(json => {
+        if (isResponseOK) {
+          console.log(json);
+          let detailed_column_list = [];
+          detailed_column_list.push({
+            dataField: 'name',
+            text: t('Name'),
+            formatter: nameFormatter,
+            sort: true
+          });
+          detailed_column_list.push({
+            dataField: 'space',
+            text: t('Space'),
+            sort: true
+          });
+          json['energycategories'].forEach((currentValue, index) => {
+            detailed_column_list.push({
+              dataField: 'a' + index,
+              text: currentValue['name'] + ' (' + currentValue['unit_of_measure'] + ')',
+              sort: true,
+              formatter: function(decimalValue) {
+                if (typeof decimalValue === 'number') {
+                  return decimalValue.toFixed(2);
+                } else {
+                  return null;
+                }
+              }
+            });
+          });
+          setDetailedDataTableColumns(detailed_column_list);
+          let shopfloors = [];
+          if (json['shopfloors'].length > 0) {
+            json['shopfloors'].forEach((currentShopfloor, index) => {
+              let detailed_value = {};
+              detailed_value['id'] = currentShopfloor['id'];
+              detailed_value['name'] = currentShopfloor['shopfloor_name'];
+              detailed_value['uuid'] = currentShopfloor['shopfloor_uuid'];
+              detailed_value['space'] = currentShopfloor['space_name'];
+              detailed_value['costcenter'] = currentShopfloor['cost_center_name'];
+              currentShopfloor['values'].forEach((currentValue, energyCategoryIndex) => {
+                detailed_value['a' + energyCategoryIndex] = currentValue;
+              });
+              shopfloors.push(detailed_value);
+            });
+          }
+
+          setShopfloorList(shopfloors);
+
+          setExcelBytesBase64(json['excel_bytes_base64']);
+
+          // enable submit button
+          setSubmitButtonDisabled(false);
+          // hide spinner
+          setSpinnerHidden(true);
+          // show export button
+          setExportButtonHidden(false);
+        } else {
+          toast.error(t(json.description));
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
 
   const handleExport = e => {
     e.preventDefault();
-    const mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    const fileName = 'shopfloorbatch.xlsx'
-    var fileUrl = "data:" + mimeType + ";base64," + excelBytesBase64;
+    const mimeType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const fileName = 'shopfloorbatch.xlsx';
+    var fileUrl = 'data:' + mimeType + ';base64,' + excelBytesBase64;
     fetch(fileUrl)
-        .then(response => response.blob())
-        .then(blob => {
-            var link = window.document.createElement('a');
-            link.href = window.URL.createObjectURL(blob, { type: mimeType });
-            link.download = fileName;
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        });
+      .then(response => response.blob())
+      .then(blob => {
+        var link = window.document.createElement('a');
+        link.href = window.URL.createObjectURL(blob, { type: mimeType });
+        link.download = fileName;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      });
   };
 
   const nameFormatter = (dataField, { name, uuid }) => (
-    <Link to={{pathname:'/shopfloor/energycategory?uuid=' + uuid}}  target = "_blank">
+    <Link to={{ pathname: '/shopfloor/energycategory?uuid=' + uuid }} target="_blank">
       <Media tag={Flex} align="center">
         <Media body className="ml-2">
           <h5 className="mb-0 fs--1">{name}</h5>
@@ -302,7 +325,8 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
     <Fragment>
       <div>
         <Breadcrumb>
-          <BreadcrumbItem>{t('Shopfloor Data')}</BreadcrumbItem><BreadcrumbItem active>{t('Batch Analysis')}</BreadcrumbItem>
+          <BreadcrumbItem>{t('Shopfloor Data')}</BreadcrumbItem>
+          <BreadcrumbItem active>{t('Batch Analysis')}</BreadcrumbItem>
         </Breadcrumb>
       </div>
       <Card className="bg-light mb-3">
@@ -315,10 +339,12 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
                     {t('Space')}
                   </Label>
                   <br />
-                  <Cascader options={cascaderOptions}
+                  <Cascader
+                    options={cascaderOptions}
                     onChange={onSpaceCascaderChange}
                     changeOnSelect
-                    expandTrigger="hover">
+                    expandTrigger="hover"
+                  >
                     <Input value={selectedSpaceName || ''} readOnly />
                   </Cascader>
                 </FormGroup>
@@ -326,8 +352,10 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
 
               <Col xs={6} sm={3}>
                 <FormGroup className="form-group">
-                  <Label className={labelClasses} for="reportingPeriodDateRangePicker">{t('Reporting Period')}</Label>
-                  <br/>
+                  <Label className={labelClasses} for="reportingPeriodDateRangePicker">
+                    {t('Reporting Period')}
+                  </Label>
+                  <br />
                   <DateRangePickerWrapper
                     id="reportingPeriodDateRangePicker"
                     format="yyyy-MM-dd HH:mm:ss"
@@ -345,31 +373,40 @@ const ShopfloorBatch = ({ setRedirect, setRedirectUrl, t }) => {
                 <FormGroup>
                   <br />
                   <ButtonGroup id="submit">
-                    <Button color="success" disabled={submitButtonDisabled} >{t('Submit')}</Button>
+                    <Button color="success" disabled={submitButtonDisabled}>
+                      {t('Submit')}
+                    </Button>
                   </ButtonGroup>
                 </FormGroup>
               </Col>
               <Col xs="auto">
                 <FormGroup>
                   <br />
-                  <Spinner color="primary" hidden={spinnerHidden}  />
+                  <Spinner color="primary" hidden={spinnerHidden} />
                 </FormGroup>
               </Col>
               <Col xs="auto">
-                  <br />
-                  <ButtonIcon icon="external-link-alt" transform="shrink-3 down-2" color="falcon-default"
+                <br />
+                <ButtonIcon
+                  icon="external-link-alt"
+                  transform="shrink-3 down-2"
+                  color="falcon-default"
                   hidden={exportButtonHidden}
-                  onClick={handleExport} >
-                    {t('Export')}
-                  </ButtonIcon>
+                  onClick={handleExport}
+                >
+                  {t('Export')}
+                </ButtonIcon>
               </Col>
             </Row>
           </Form>
         </CardBody>
       </Card>
-      <DetailedDataTable data={shopfloorList} title={t('Detailed Data')} columns={detailedDataTableColumns} pagesize={50} >
-      </DetailedDataTable>
-
+      <DetailedDataTable
+        data={shopfloorList}
+        title={t('Detailed Data')}
+        columns={detailedDataTableColumns}
+        pagesize={50}
+      />
     </Fragment>
   );
 };
