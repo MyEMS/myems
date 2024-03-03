3446ef070c7aac3df3da3b5b0c2d386c56e54c34
nengyuanzhang <13011132526@163.com>
2024-03-03 14:51:33 +0800

    fixed warnings in myems-web

--- myems-web/src/components/MyEMS/Monitoring/SpaceEquipments.js
+++ myems-web/src/components/MyEMS/Monitoring/SpaceEquipments.js
@@ -13,7 +13,7 @@ import {
   FormGroup,
   Input,
   Label,
-  Spinner,
+  Spinner
 } from 'reactstrap';
 import Loader from '../../common/Loader';
 import useFakeFetch from '../../../hooks/useFakeFetch';
@@ -31,7 +31,6 @@ import { withTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { APIBaseURL, settings } from '../../../config';
 
-
 const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
   useEffect(() => {
     let is_logged_in = getCookieValue('is_logged_in');
@@ -39,7 +38,7 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
     let user_display_name = getCookieValue('user_display_name');
     let user_uuid = getCookieValue('user_uuid');
     let token = getCookieValue('token');
-    if (checkEmpty(is_logged_in) || checkEmpty(token)|| checkEmpty(user_uuid) || !is_logged_in) {
+    if (checkEmpty(is_logged_in) || checkEmpty(token) || checkEmpty(user_uuid) || !is_logged_in) {
       setRedirectUrl(`/authentication/basic/login`);
       setRedirect(true);
     } else {
@@ -82,30 +81,37 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
         'User-UUID': getCookieValue('user_uuid'),
         Token: getCookieValue('token')
       },
-      body: null,
-
-    }).then(response => {
-      console.log(response);
-      if (response.ok) {
-        isResponseOK = true;
-      }
-      return response.json();
-    }).then(json => {
-      console.log(json);
-      if (isResponseOK) {
-        // rename keys
-        json = JSON.parse(JSON.stringify([json]).split('"id":').join('"value":').split('"name":').join('"label":'));
-        setCascaderOptions(json);
-        setSelectedSpaceName([json[0]].map(o => o.label));
-        setSelectedSpaceID([json[0]].map(o => o.value));
-      } else {
-        toast.error(t(json.description));
-      }
-    }).catch(err => {
-      console.log(err);
-    });
-
-  }, []);
+      body: null
+    })
+      .then(response => {
+        console.log(response);
+        if (response.ok) {
+          isResponseOK = true;
+        }
+        return response.json();
+      })
+      .then(json => {
+        console.log(json);
+        if (isResponseOK) {
+          // rename keys
+          json = JSON.parse(
+            JSON.stringify([json])
+              .split('"id":')
+              .join('"value":')
+              .split('"name":')
+              .join('"label":')
+          );
+          setCascaderOptions(json);
+          setSelectedSpaceName([json[0]].map(o => o.label));
+          setSelectedSpaceID([json[0]].map(o => o.value));
+        } else {
+          toast.error(t(json.description));
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [t]);
 
   const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
 
@@ -155,7 +161,8 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
     <Fragment>
       <div>
         <Breadcrumb>
-          <BreadcrumbItem>{t('Monitoring')}</BreadcrumbItem><BreadcrumbItem active>{t('Space Equipments')}</BreadcrumbItem>
+          <BreadcrumbItem>{t('Monitoring')}</BreadcrumbItem>
+          <BreadcrumbItem active>{t('Space Equipments')}</BreadcrumbItem>
         </Breadcrumb>
       </div>
       <Card className="bg-light mb-3">
@@ -168,10 +175,12 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
                     {t('Space')}
                   </Label>
                   <br />
-                  <Cascader options={cascaderOptions}
+                  <Cascader
+                    options={cascaderOptions}
                     onChange={onSpaceCascaderChange}
                     changeOnSelect
-                    expandTrigger="hover">
+                    expandTrigger="hover"
+                  >
                     <Input value={selectedSpaceName || ''} readOnly />
                   </Cascader>
                 </FormGroup>
@@ -180,14 +189,16 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
                 <FormGroup>
                   <br />
                   <ButtonGroup id="submit">
-                    <Button color="success" disabled={submitButtonDisabled} >{t('Submit')}</Button>
+                    <Button color="success" disabled={submitButtonDisabled}>
+                      {t('Submit')}
+                    </Button>
                   </ButtonGroup>
                 </FormGroup>
               </Col>
               <Col xs="auto">
                 <FormGroup>
                   <br />
-                  <Spinner color="primary" hidden={spinnerHidden}  />
+                  <Spinner color="primary" hidden={spinnerHidden} />
                 </FormGroup>
               </Col>
             </Row>
@@ -195,19 +206,20 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
         </CardBody>
       </Card>
 
-
       <Card>
         <CardBody className={classNames({ 'p-0  overflow-hidden': isList, 'pb-0': isGrid })}>
           {loading ? (
             <Loader />
           ) : (
-              <Row noGutters={isList}>
-                {isIterableArray(equipments) &&
-                  equipments
-                    .filter(equipment => paginationData.includes(equipment.id))
-                    .map((equipment, index) => <EquipmentList {...equipment} sliderSettings={sliderSettings} key={equipment.id} index={index} />)}
-              </Row>
-            )}
+            <Row noGutters={isList}>
+              {isIterableArray(equipments) &&
+                equipments
+                  .filter(equipment => paginationData.includes(equipment.id))
+                  .map((equipment, index) => (
+                    <EquipmentList {...equipment} sliderSettings={sliderSettings} key={equipment.id} index={index} />
+                  ))}
+            </Row>
+          )}
         </CardBody>
         <EquipmentFooter meta={paginationMeta} handler={paginationHandler} />
       </Card>
@@ -215,9 +227,7 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
         <CardBody>
           <Row className="justify-content-between align-items-center">
             <Col sm="auto" className="mb-2 mb-sm-0" tag={Flex} align="center">
-              <h6 className="mb-0 text-nowrap ml-2">
-                {t('Show Up to')}
-              </h6>
+              <h6 className="mb-0 text-nowrap ml-2">{t('Show Up to')}</h6>
               <CustomInput
                 id="itemsPerPage"
                 type="select"
@@ -231,10 +241,9 @@ const SpaceEquipments = ({ setRedirect, setRedirectUrl, t }) => {
                 <option value={total}>{t('All')}</option>
               </CustomInput>
               <h6 className="mb-0 text-nowrap ml-2">
-                {t('FROM - TO of TOTAL Equipments', { 'FROM': from, 'TO': to, 'TOTAL': total })}
+                {t('FROM - TO of TOTAL Equipments', { FROM: from, TO: to, TOTAL: total })}
               </h6>
             </Col>
-
           </Row>
         </CardBody>
       </Card>
