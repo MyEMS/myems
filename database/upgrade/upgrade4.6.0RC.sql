bce72f88faaea007ced4edb347523e629d4e132f
nengyuanzhang <13011132526@163.com>
2024-05-27 22:06:26 +0800

    added columns to tbl_microgrids_power_conversion_systems in database added columns to tbl_energy_storage_containers_power_conversion_systems in database

--- database/upgrade/upgrade4.6.0RC.sql
+++ database/upgrade/upgrade4.6.0RC.sql
@@ -16,15 +16,15 @@ CREATE TABLE IF NOT EXISTS `myems_historical_db`.`tbl_energy_plan_files` (
   PRIMARY KEY (`id`));
 
 
-ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `total_discharge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `total_charge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `today_discharge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `today_charge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `total_discharge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `total_charge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `today_discharge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_microgrids_power_conversion_systems ADD `today_charge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
 
-ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `total_discharge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `total_charge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `today_discharge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
-ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `today_charge_energy` DECIMAL(18, 3) NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `total_discharge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `total_charge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `today_discharge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
+ALTER TABLE myems_system_db.tbl_energy_storage_containers_power_conversion_systems ADD `today_charge_energy_point_id` BIGINT NOT NULL AFTER `rated_output_power`;
 
 -- UPDATE VERSION NUMBER
 UPDATE `myems_system_db`.`tbl_versions` SET version='4.6.0RC', release_date='2024-06-18' WHERE id=1;
