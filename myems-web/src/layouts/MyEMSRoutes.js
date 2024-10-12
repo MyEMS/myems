f4a22a7983909de6a361d366e82f199bc6e30585
caozhenhui <823914102@qq.com>
2024-10-12 14:35:25 +0800

    added production energy consumption report

--- myems-web/src/layouts/MyEMSRoutes.js
+++ myems-web/src/layouts/MyEMSRoutes.js
@@ -116,6 +116,8 @@ import SpaceSaving from '../components/MyEMS/Space/SpaceSaving';
 import SpacePlan from '../components/MyEMS/Space/SpacePlan';
 import SpaceEnvironmentMonitor from '../components/MyEMS/Space/SpaceEnvironmentMonitor';
 import EnterProduction from '../components/MyEMS/Space/EnterProduction';
+import ProductionEnergyConsumption from '../components/MyEMS/Space/ProductionEnergyConsumption';
+
 // Equipment
 import EquipmentBatch from '../components/MyEMS/Equipment/EquipmentBatch';
 import EquipmentCarbon from '../components/MyEMS/Equipment/EquipmentCarbon';
@@ -372,6 +374,7 @@ const MyEMSRoutes = () => (
     <Route path="/space/plan" exact component={SpacePlan} />
     <Route path="/space/environmentmonitor" exact component={SpaceEnvironmentMonitor} />
     <Route path="/space/enterproduction" exact component={EnterProduction} />
+    <Route path="/space/productionenergy" exact component={ProductionEnergyConsumption} />
 
     {/*Equipment*/}
     <Route path="/equipment/batch" exact component={EquipmentBatch} />
