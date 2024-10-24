a4aa185208b57c1fa0a939d12c5361f01ee3de58
nengyuanzhang <13011132526@163.com>
2024-10-24 16:39:47 +0800

    added space cascader to energy flow diagram in myems-web

--- myems-web/src/components/MyEMS/AuxiliarySystem/EnergyFlowDiagram.js
+++ myems-web/src/components/MyEMS/AuxiliarySystem/EnergyFlowDiagram.js
@@ -108,94 +108,94 @@ const EnergyFlowDiagram = ({ setRedirect, setRedirectUrl, t }) => {
   //Results
   const [energyFlowDiagramData, setEnergyFlowDiagramData] = useState({ nodes: [], links: [] });
   const [excelBytesBase64, setExcelBytesBase64] = useState(undefined);
-  const [detailedDataTableColumns, setDetailedDataTableColumns] = useState([
-    { dataField: 'startdatetime', text: t('Datetime'), sort: true }
-  ]);
-  const [detailedDataTableData, setDetailedDataTableData] = useState([]);
+
   useEffect(() => {
     let isResponseOK = false;
-    fetch(APIBaseURL + '/spaces/tree', {
-      method: 'GET',
-      headers: {
-        'Content-type': 'application/json',
-        'User-UUID': getCookieValue('user_uuid'),
-        Token: getCookieValue('token')
-      },
-      body: null
-    })
-      .then(response => {
-        console.log(response);
-        if (response.ok) {
-          isResponseOK = true;
-        }
-        return response.json();
+      fetch(APIBaseURL + '/spaces/tree', {
+        method: 'GET',
+        headers: {
+          'Content-type': 'application/json',
+          'User-UUID': getCookieValue('user_uuid'),
+          Token: getCookieValue('token')
+        },
+        body: null
       })
-      .then(json => {
-        console.log(json);
-        if (isResponseOK) {
-          // rename keys
-          json = JSON.parse(
-            JSON.stringify(json)
-              .split('"id":')
-              .join('"value":')
-              .split('"name":')
-              .join('"label":')
-          );
-          setCascaderOptions(json);
-          setSelectedSpaceName([json[0]].map(o => o.label));
-          setSelectedSpaceID([json[0]].map(o => o.value));
-          let isResponseOK = false;
-          fetch(APIBaseURL + '/spaces/' + [json[0]].map(o => o.value) + '/energyflowdiagrams', {
-            method: 'GET',
-            headers: {
-              'Content-type': 'application/json',
-              'User-UUID': getCookieValue('user_uuid'),
-              Token: getCookieValue('token')
-            },
-            body: null
-          })
-            .then(response => {
-              if (response.ok) {
-                isResponseOK = true;
-              }
-              return response.json();
+        .then(response => {
+          console.log(response);
+          if (response.ok) {
+            isResponseOK = true;
+          }
+          return response.json();
+        })
+        .then(json => {
+          console.log(json);
+          if (isResponseOK) {
+            // rename keys
+            json = JSON.parse(
+              JSON.stringify([json])
+                .split('"id":')
+                .join('"value":')
+                .split('"name":')
+                .join('"label":')
+            );
+            setCascaderOptions(json);
+            setSelectedSpaceName([json[0]].map(o => o.label));
+            setSelectedSpaceID([json[0]].map(o => o.value));
+            // get EnergyFlowDiagrams by root Space ID
+            let isResponseOK = false;
+            fetch(APIBaseURL + '/spaces/' + [json[0]].map(o => o.value) + '/energyflowdiagrams', {
+              method: 'GET',
+              headers: {
+                'Content-type': 'application/json',
+                'User-UUID': getCookieValue('user_uuid'),
+                Token: getCookieValue('token')
+              },
+              body: null
             })
-            .then(json => {
-              if (isResponseOK) {
-                json = JSON.parse(
-                  JSON.stringify([json])
-                    .split('"id":')
-                    .join('"value":')
-                    .split('"name":')
-                    .join('"label":')
-                );
-                console.log(json);
-                setEnergyFlowDiagramList(json[0]);
-                if (json[0].length > 0) {
-                  setSelectedEnergyFlowDiagram(json[0][0].value);
-                  // enable submit button
-                  setSubmitButtonDisabled(false);
-                } else {
-                  setSelectedEnergyFlowDiagram(undefined);
-                  // disable submit button
-                  setSubmitButtonDisabled(true);
+              .then(response => {
+                if (response.ok) {
+                  isResponseOK = true;
                 }
-              } else {
-                toast.error(t(json.description));
-              }
-            })
-            .catch(err => {
-              console.log(err);
-            });
-          // end of get EnergyFlowDiagrams by root Space ID
-        } else {
-          toast.error(t(json.description));
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, [t]);
+                return response.json();
+              })
+              .then(json => {
+                if (isResponseOK) {
+                  json = JSON.parse(
+                    JSON.stringify([json])
+                      .split('"id":')
+                      .join('"value":')
+                      .split('"name":')
+                      .join('"label":')
+                  );
+                  console.log(json);
+                  setEnergyFlowDiagramList(json[0]);
+                  if (json[0].length > 0) {
+                    setSelectedEnergyFlowDiagram(json[0][0].value);
+                    // enable submit button
+                    setSubmitButtonDisabled(false);
+                  } else {
+                    setSelectedEnergyFlowDiagram(undefined);
+                    // disable submit button
+                    setSubmitButtonDisabled(true);
+                    // hide export button
+                    setExportButtonHidden(true);
+                  }
+                } else {
+                  toast.error(t(json.description));
+                }
+              })
+              .catch(err => {
+                console.log(err);
+              });
+            // end of get EnergyFlowDiagrams by root Space ID
+          } else {
+            toast.error(t(json.description));
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+      }, []);
 
   const labelClasses = 'ls text-uppercase text-600 font-weight-semi-bold mb-0';
 
@@ -238,6 +238,8 @@ const EnergyFlowDiagram = ({ setRedirect, setRedirectUrl, t }) => {
             setSelectedEnergyFlowDiagram(undefined);
             // disable submit button
             setSubmitButtonDisabled(true);
+            // hide export button
+            setExportButtonHidden(true);
           }
         } else {
           toast.error(t(json.description));
@@ -380,9 +382,6 @@ const EnergyFlowDiagram = ({ setRedirect, setRedirectUrl, t }) => {
     // hide result data
     setResultDataHidden(true);
 
-    // Reinitialize tables
-    setDetailedDataTableData([]);
-
     let isResponseOK = false;
     fetch(
       APIBaseURL +
@@ -417,7 +416,9 @@ const EnergyFlowDiagram = ({ setRedirect, setRedirectUrl, t }) => {
 
           let detial_value_list = [];
           // choose the first point's timestamps (not empty) for all points
-          if (json['reporting_period']['timestamps'].length > 0) {
+          if (json['reporting_period'] !== undefined &&
+          json['reporting_period']['timestamps'] !== undefined &&
+          json['reporting_period']['timestamps'].length > 0) {
             let arr_index = 0;
             for (let index in json['reporting_period']['timestamps']) {
               if (json['reporting_period']['timestamps'][index].length === 0) {
@@ -438,10 +439,6 @@ const EnergyFlowDiagram = ({ setRedirect, setRedirectUrl, t }) => {
               });
             }
           }
-
-          setTimeout(() => {
-            setDetailedDataTableData(detial_value_list);
-          }, 0);
 
           setExcelBytesBase64(json['excel_bytes_base64']);
 
