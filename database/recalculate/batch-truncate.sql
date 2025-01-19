a33d567e2dac37012f99833ccfe31d4c41466480
nengyuanzhang <13011132526@163.com>
2025-01-19 15:39:23 +0800

    updated data recalculating scripts in database

--- database/recalculate/batch-truncate.sql
+++ database/recalculate/batch-truncate.sql
@@ -1,15 +1,24 @@
 
 -- NOTE: this script is DANGEROUS and may cause data loss so it is for advanced users only
+-- 注意: 这个脚本很危险，可能会造成数据丢失，仅限高级用户使用
 -- NOTE: before running this script, you should stop the myems-normalization service and myems-aggregation service
+-- 注意：运行这个脚本前必须停止myems-normalization服务和myems-aggregation服务
 -- NOTE  after running this script, you should start the myems-normalization service and myems-aggregation service
+-- 注意：运行这个脚本后必须启动myems-normalization服务和myems-aggregation服务
 
 TRUNCATE TABLE myems_energy_db.tbl_combined_equipment_input_category_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_combined_equipment_input_item_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_combined_equipment_output_category_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_energy_storage_container_charge_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_energy_storage_container_discharge_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_container_grid_buy_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_container_grid_sell_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_container_load_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_energy_storage_power_station_charge_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_energy_storage_power_station_discharge_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_energy_storage_power_station_load_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_equipment_input_category_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_equipment_input_item_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_equipment_output_category_hourly;
@@ -22,7 +31,12 @@ TRUNCATE TABLE myems_energy_db.tbl_microgrid_grid_sell_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_microgrid_load_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_microgrid_photovoltaic_hourly;
 -- NOTE: if you truncate tbl_offline_meter_hourly, the offline meter files should be reuploaded
+-- 注意: 如果删除tbl_offline_meter_hourly数据，离线表文件必须重新上传
 -- TRUNCATE TABLE myems_energy_db.tbl_offline_meter_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_photovoltaic_power_station_generation_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_photovoltaic_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_photovoltaic_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_energy_db.tbl_photovoltaic_power_station_load_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_shopfloor_input_category_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_shopfloor_input_item_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_space_input_category_hourly;
@@ -34,13 +48,21 @@ TRUNCATE TABLE myems_energy_db.tbl_tenant_input_category_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_tenant_input_item_hourly;
 TRUNCATE TABLE myems_energy_db.tbl_virtual_meter_hourly;
 
+
+TRUNCATE TABLE myems_billing_db.tbl_combined_equipment_input_category_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_combined_equipment_input_category_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_combined_equipment_input_item_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_combined_equipment_output_category_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_energy_storage_container_charge_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_energy_storage_container_discharge_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_container_grid_buy_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_container_grid_sell_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_container_load_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_energy_storage_power_station_charge_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_energy_storage_power_station_discharge_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_energy_storage_power_station_load_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_equipment_input_category_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_equipment_input_item_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_equipment_output_category_hourly;
@@ -53,6 +75,10 @@ TRUNCATE TABLE myems_billing_db.tbl_microgrid_grid_sell_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_microgrid_load_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_microgrid_photovoltaic_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_offline_meter_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_photovoltaic_power_station_generation_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_photovoltaic_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_photovoltaic_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_billing_db.tbl_photovoltaic_power_station_load_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_shopfloor_input_category_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_shopfloor_input_item_hourly;
 TRUNCATE TABLE myems_billing_db.tbl_space_input_category_hourly;
@@ -69,8 +95,14 @@ TRUNCATE TABLE myems_carbon_db.tbl_combined_equipment_input_item_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_combined_equipment_output_category_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_container_charge_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_container_discharge_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_container_grid_buy_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_container_grid_sell_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_container_load_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_power_station_charge_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_power_station_discharge_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_energy_storage_power_station_load_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_equipment_input_category_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_equipment_input_item_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_equipment_output_category_hourly;
@@ -83,6 +115,10 @@ TRUNCATE TABLE myems_carbon_db.tbl_microgrid_grid_sell_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_microgrid_load_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_microgrid_photovoltaic_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_offline_meter_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_photovoltaic_power_station_generation_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_photovoltaic_power_station_grid_buy_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_photovoltaic_power_station_grid_sell_hourly;
+TRUNCATE TABLE myems_carbon_db.tbl_photovoltaic_power_station_load_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_shopfloor_input_category_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_shopfloor_input_item_hourly;
 TRUNCATE TABLE myems_carbon_db.tbl_space_input_category_hourly;
