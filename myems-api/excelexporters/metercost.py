35d14467a88b8add41c167ac4b53c56fd1543937
Lurkisy <979233767@qq.com>
2025-02-23 19:21:13 +0800

    updated in myems-api/excelexporters/

--- myems-api/excelexporters/metercost.py
+++ myems-api/excelexporters/metercost.py
@@ -521,16 +521,13 @@ def generate_excel(report,
                 line.add_data(base_line_data, titles_from_data=True)
                 line.add_data(reporting_line_data, titles_from_data=True)
                 line.set_categories(labels)
+                line.dLbls = None
                 for j in range(len(line.series)):
                     line.series[j].marker.symbol = "auto"
                     line.series[j].smooth = True
                 line.x_axis.crosses = 'min'
                 line.height = 8.25
                 line.width = 24
-                line.dLbls = DataLabelList()
-                line.dLbls.dLblPos = 't'
-                line.dLbls.showVal = False
-                line.dLbls.showPercent = False
                 chart_col = 'B'
                 chart_cell = chart_col + str(table_start_draw_flag)
                 ws.add_chart(line, chart_cell)
