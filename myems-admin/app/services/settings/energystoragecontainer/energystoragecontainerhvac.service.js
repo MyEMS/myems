de4fd336e5aaa96abfade50e112aa25994040b4e
nengyuanzhang <13011132526@163.com>
2025-03-18 13:36:57 +0800

    udpated power station

--- myems-admin/app/services/settings/energystoragecontainer/energystoragecontainerhvac.service.js
+++ myems-admin/app/services/settings/energystoragecontainer/energystoragecontainerhvac.service.js
@@ -17,6 +17,30 @@ app.factory('EnergyStorageContainerHVACService', function($http) {
                 callback(response);
             });
         },
+        addEnergyStorageContainerHVAC: function(id, energystoragecontainerhvac, headers, callback) {
+            $http.post(getAPI()+'energystoragecontainers/'+id+'/hvacs',{data:energystoragecontainerhvac}, {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        editEnergyStorageContainerHVAC: function(id, energystoragecontainerhvac, headers, callback) {
+            $http.put(getAPI()+'energystoragecontainers/'+id+'/hvacs/'+energystoragecontainerhvac.id,{data:energystoragecontainerhvac}, {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
+        deleteEnergyStorageContainerHVAC: function(id, energystoragecontainerhvacID, headers, callback) {
+            $http.delete(getAPI()+'energystoragecontainers/'+id+'/hvacs/'+energystoragecontainerhvacID, {headers})
+            .then(function (response) {
+                callback(response);
+            }, function (response) {
+                callback(response);
+            });
+        },
         addPair: function(id, hid, pid, headers, callback) {
             $http.post(getAPI() + 'energystoragecontainers/' + id + '/hvacs/' + hid + '/points', {data:{'point_id':pid}}, {headers})
             .then(function (response) {
