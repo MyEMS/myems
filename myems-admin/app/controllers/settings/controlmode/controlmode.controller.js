137e7d1a32332dd07f320f3160f42e99e08d024c
panskai <2404093075@qq.com>
2025-07-22 11:27:23 +0800

    fixed issue where end time could be earlier than start time in control mode (myems-admin)

--- myems-admin/app/controllers/settings/controlmode/controlmode.controller.js
+++ myems-admin/app/controllers/settings/controlmode/controlmode.controller.js
@@ -239,7 +239,7 @@ app.controller('ControlModeController', function(
 
 });
 
-app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalInstance, params, toaster, $translate) {
+app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalInstance, params, $translate) {
 
 	$scope.operation = "SETTING.ADD_CONTROL_MODE";
 	$scope.disable=false;
@@ -270,19 +270,21 @@ app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalIn
 		singleDatePicker: true,
 	};
 
+	$scope.error = {
+		show: false,
+		message: ''
+	};
+
 	$scope.ok = function() {
 		for (var i = 0; i < $scope.times.length; i++) {
         	var item = $scope.times[i];
         	if (item.end_time_of_day < item.start_time_of_day) {
-				toaster.pop({
-					type: "error",
-					title: $translate.instant("TOASTER.ADD_FAILURE"),
-					body: $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME"),
-					showCloseButton: true,
-				});
+				$scope.error.show = true;
+				$scope.error.message = $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME");
             	return;
         	}
     	}
+		scope.error.show = false;
 		$scope.controlmode.times=$scope.times;
 		$uibModalInstance.close($scope.controlmode);
 	};
@@ -290,6 +292,8 @@ app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalIn
 	$scope.cancel = function() {
 		$uibModalInstance.dismiss('cancel');
 	};
+
+
 	$scope.add = function(t) {
 		if (t.power_value == null){
 			return false;
@@ -298,14 +302,13 @@ app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalIn
 		t.end_time_of_day= t.end_hour + ':' + t.end_min + ':' + t.end_second;
 
 		if (t.end_time_of_day < t.start_time_of_day) {
-			toaster.pop({
-				type: "error",
-				title: $translate.instant("TOASTER.ADD_FAILURE"),
-				body: $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME"),
-				showCloseButton: true,
-			});
+			$scope.error.show = true;
+			$scope.error.message = $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME");
 			return;
 		}
+
+		$scope.error.show = false;
+    	$scope.error.message = '';
 
 		if ($scope.times.length > 0) {
 			$scope.times.unshift(angular.copy(t));
@@ -333,7 +336,7 @@ app.controller('ModalAddControlModeCtrl', function($scope, $timeout, $uibModalIn
 	};
 });
 
-app.controller('ModalEditControlModeCtrl', function($scope, $timeout, $uibModalInstance, params) {
+app.controller('ModalEditControlModeCtrl', function($scope, $timeout, $uibModalInstance, params, $translate) {
 	$scope.operation = "SETTING.EDIT_CONTROL_MODE";
 	$scope.disable=true;
 	$scope.controlmode = params.controlmode;
@@ -363,7 +366,23 @@ app.controller('ModalEditControlModeCtrl', function($scope, $timeout, $uibModalI
 		angular.element('#touTable').trigger('footable_redraw');
 	}, 100);
 
+	$scope.error = {
+    	show: false,
+    	message: ''
+	};
+
 	$scope.ok = function() {
+		$scope.error.show = false;
+    	for (var i = 0; i < $scope.times.length; i++) {
+        	var item = $scope.times[i];
+        	if (item.end_time_of_day < item.start_time_of_day) {
+            	$scope.error.show = true;
+            	$scope.error.message = $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME");
+            	return;
+        	}
+    	}
+
+    	$scope.controlmode.times = $scope.times;
 		$uibModalInstance.close($scope.controlmode);
 	};
 
@@ -377,6 +396,16 @@ app.controller('ModalEditControlModeCtrl', function($scope, $timeout, $uibModalI
 		}
 		t.start_time_of_day= t.start_hour + ':' + t.start_min + ':' + t.start_second;
 		t.end_time_of_day= t.end_hour + ':' + t.end_min + ':' + t.end_second;
+
+		if (t.end_time_of_day < t.start_time_of_day) {
+        	$scope.error.show = true;
+        	$scope.error.message = $translate.instant("SETTING.END_TIME_SHOULD_BE_AFTER_START_TIME");
+        	return;
+    	}
+
+    	$scope.error.show = false;
+    	$scope.error.message = '';
+
 		if ($scope.times.length > 0) {
 			$scope.times.unshift(angular.copy(t));
 		} else {
