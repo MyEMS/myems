aecc677be1e8fd0e9c9f2a7cd65787eaa6cb32b6
zhan <15734588+zhanyujie_1@user.noreply.gitee.com>
2025-09-19 17:01:51 +0800

    solve the issue of detailed words when deleted

--- myems-api/core/meter.py
+++ myems-api/core/meter.py
@@ -504,8 +504,8 @@ class MeterItem:
                        " FROM tbl_variables va, tbl_virtual_meters vm "
                        " WHERE va.meter_id = %s AND va.meter_type = 'meter' AND va.virtual_meter_id = vm.id ",
                        (id_,))
-        row_virtual_meter = cursor.fetchall()
-        if row_virtual_meter is not None and len(row_virtual_meter)>0:
+        rows_virtual_meters = cursor.fetchall()
+        if rows_virtual_meters is not None and len(rows_virtual_meters)>0:
             cursor.close()
             cnx.close()
             raise falcon.HTTPError(status=falcon.HTTP_400,
