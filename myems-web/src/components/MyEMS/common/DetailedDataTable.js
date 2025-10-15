942b26ffa33690ac492172ef1c533347306498cf
panskai <7679016+xiaochang_chaika@user.noreply.gitee.com>
2025-10-15 21:16:17 +0800

    fixed the pagination display issue of the meter ledger in myems-web

--- myems-web/src/components/MyEMS/common/DetailedDataTable.js
+++ myems-web/src/components/MyEMS/common/DetailedDataTable.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { Button, Card, CardBody, Row, Col } from 'reactstrap';
 import FalconCardHeader from '../../common/FalconCardHeader';
 import { withTranslation } from 'react-i18next';
-import paginationFactory, { PaginationProvider } from 'react-bootstrap-table2-paginator';
+// Removed react-bootstrap-table2-paginator to avoid page reset flicker
 import { getPaginationArray } from '../../../helpers/utils';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -14,80 +14,81 @@ const defaultSorted = [
   }
 ];
 
-const DetailedDataTable = ({ title, data, columns, pagesize, t }) => {
-  const options = {
-    custom: true,
-    sizePerPage: pagesize,
-    totalSize: data.length
+const DetailedDataTable = ({ title, data, columns, pagesize, t, page: controlledPage, onChangePage }) => {
+  // Persist current page to prevent flicker/jumps when parent re-renders
+  const [uncontrolledPage, setUncontrolledPage] = useState(1);
+  const page = controlledPage ?? uncontrolledPage;
+  const setPage = (p) => {
+    if (onChangePage) {
+      onChangePage(p);
+    } else {
+      setUncontrolledPage(p);
+    }
   };
-  const handleNextPage = ({ page, onPageChange }) => () => {
-    onPageChange(page + 1);
-  };
-
-  const handlePrevPage = ({ page, onPageChange }) => () => {
-    onPageChange(page - 1);
-  };
+  const totalSize = data.length;
+  const totalPages = useMemo(() => Math.max(1, Math.ceil(totalSize / pagesize)), [totalSize, pagesize]);
+  useEffect(() => {
+    if (totalSize > 0) {
+      if (page > totalPages) {
+        setPage(totalPages);
+      }
+    }
+  }, [totalSize, totalPages, page]);
+  const firstIndex = (page - 1) * pagesize;
+  const lastIndex = Math.min(firstIndex + pagesize, totalSize);
+  const pageData = useMemo(() => data.slice(firstIndex, lastIndex), [data, firstIndex, lastIndex]);
+  // Use local calculations; never mutate pagination props
   return (
     <Fragment>
       <Card>
         <FalconCardHeader title={title} className="bg-light" titleClass="text-lightSlateGray mb-0" />
         <CardBody>
-          <PaginationProvider pagination={paginationFactory(options)}>
-            {({ paginationProps, paginationTableProps }) => {
-              const lastIndex = paginationProps.page * paginationProps.sizePerPage;
-              while ((paginationProps.page - 1) * paginationProps.sizePerPage >= paginationProps.totalSize) {
-                paginationProps.page = paginationProps.page - 1;
-              }
-              return (
-                <Fragment>
-                  <Row>
-                    <Col>
-                      <BootstrapTable
-                        bootstrap4
-                        keyField="id"
-                        data={data}
-                        columns={columns}
-                        defaultSorted={defaultSorted}
-                        {...paginationTableProps}
-                      />
-                    </Col>
-                  </Row>
-                  <Row noGutters className="px-1 py-3 flex-center">
-                    <Col xs="auto">
-                      <Button
-                        color="falcon-default"
-                        size="sm"
-                        onClick={handlePrevPage(paginationProps)}
-                        disabled={paginationProps.page === 1}
-                      >
-                        <FontAwesomeIcon icon="chevron-left" />
-                      </Button>
-                      {getPaginationArray(paginationProps.totalSize, paginationProps.sizePerPage).map(pageNo => (
-                        <Button
-                          color={paginationProps.page === pageNo ? 'falcon-primary' : 'falcon-default'}
-                          size="sm"
-                          className="ml-2"
-                          onClick={() => paginationProps.onPageChange(pageNo)}
-                          key={pageNo}
-                        >
-                          {pageNo}
-                        </Button>
-                      ))}
-                      <Button
-                        color="falcon-default"
-                        size="sm"
-                        className="ml-2"
-                        onClick={handleNextPage(paginationProps)}
-                        disabled={lastIndex >= paginationProps.totalSize}
-                      >
-                        <FontAwesomeIcon icon="chevron-right" />
-                      </Button>
-                    </Col>
-                  </Row>
-                </Fragment>
-              );
-            }}
-          </PaginationProvider>
+          <Row>
+            <Col>
+              <BootstrapTable
+                bootstrap4
+                keyField="id"
+                data={pageData}
+                columns={columns}
+                defaultSorted={defaultSorted}
+              />
+            </Col>
+          </Row>
+          <Row noGutters className="px-1 py-3 flex-center">
+            <Col xs="auto">
+              <Button
+                color="falcon-default"
+                size="sm"
+                type="button"
+                onClick={() => setPage(Math.max(1, page - 1))}
+                disabled={page === 1}
+              >
+                <FontAwesomeIcon icon="chevron-left" />
+              </Button>
+              {getPaginationArray(totalSize, pagesize).map(pageNo => (
+                <Button
+                  color={page === pageNo ? 'falcon-primary' : 'falcon-default'}
+                  size="sm"
+                  className="ml-2"
+                  type="button"
+                  onClick={() => setPage(pageNo)}
+                  key={pageNo}
+                >
+                  {pageNo}
+                </Button>
+              ))}
+              <Button
+                color="falcon-default"
+                size="sm"
+                className="ml-2"
+                type="button"
+                onClick={() => setPage(Math.min(totalPages, page + 1))}
+                disabled={page >= totalPages}
+              >
+                <FontAwesomeIcon icon="chevron-right" />
+              </Button>
+            </Col>
+          </Row>
         </CardBody>
       </Card>
     </Fragment>
