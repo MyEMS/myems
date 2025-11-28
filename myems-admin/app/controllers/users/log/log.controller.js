ed1bc19abff916c8958f9a0fa657e7522ba24a7d
panskai <7679016+xiaochang_chaika@user.noreply.gitee.com>
2025-11-28 20:29:16 +0800

    Add log display feature in mymes-admin

--- myems-admin/app/controllers/users/log/log.controller.js
+++ myems-admin/app/controllers/users/log/log.controller.js
@@ -9,14 +9,32 @@ app.controller('LogController', function (
     $scope.cur_user = JSON.parse($window.localStorage.getItem("myems_admin_ui_current_user"));
 
     $scope.logs = [];
+    $scope.paginatedLogs = [];
+    $scope.paging = {
+        currentPage: 1,
+        pageSize: 15
+    };
+
+    $scope.updatePaginatedLogs = function () {
+        var begin = ($scope.paging.currentPage - 1) * $scope.paging.pageSize;
+        var end = begin + $scope.paging.pageSize;
+        $scope.paginatedLogs = $scope.logs.slice(begin, end);
+    };
+
+    $scope.pageChanged = function () {
+        $scope.updatePaginatedLogs();
+    };
 
     $scope.getLogs = function () {
         let headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
         LogService.getLogs(headers, function (response) {
             if (angular.isDefined(response.status) && response.status === 200) {
-                $scope.logs = response.data;
+                $scope.logs = angular.isArray(response.data) ? response.data : [];
+                $scope.paging.currentPage = 1;
+                $scope.updatePaginatedLogs();
             } else {
                 $scope.logs = [];
+                $scope.updatePaginatedLogs();
                 if (response.data && response.data.description) {
                     toaster.pop({
                         type: "error",
