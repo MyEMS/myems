a684d0d53b835884e0414c550e0e4bf53e6f3ed0
gongtuo <2209581692@qq.com>
2025-12-06 09:56:53 +0800

    Add the function of batch addition for Saturdays and Sundays

--- myems-admin/app/controllers/settings/workingcalendar/workingcalendarnonworkingday.controller.js
+++ myems-admin/app/controllers/settings/workingcalendar/workingcalendarnonworkingday.controller.js
@@ -19,6 +19,10 @@ app.controller('WorkingCalendarNonWorkingDayController', function (
     $scope.nonWorkingDaysFlagArray = [],
 
     $scope.getNonWorkingDaysByWorkingCalendarID = function (id) {
+        if (!id || id === 'undefined') {
+            $scope.nonworkingdays = [];
+            return;
+        }
         let headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
         WorkingCalendarNonWorkingDayService.getNonWorkingDaysByWorkingCalendarID(id, headers, function (response) {
             if (angular.isDefined(response.status) && response.status === 200) {
@@ -28,100 +32,30 @@ app.controller('WorkingCalendarNonWorkingDayController', function (
                 $scope.nonworkingdays = [];
             }
         });
-
     };
 
     $scope.changeWorkingCalendar=function(item,model){
   	  $scope.currentWorkingCalendar=item;
   	  $scope.currentWorkingCalendar.selected=model;
-  	  $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
+  	  $scope.getNonWorkingDaysByWorkingCalendarID(item ? item.id : null);
     };
-
 
     $scope.getAllWorkingCalendars = function () {
         let headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
         WorkingCalendarService.getAllWorkingCalendars(headers, function (response) {
             if (angular.isDefined(response.status) && response.status === 200) {
                 $scope.workingcalendars = response.data;
-                $timeout(function () {
-                    $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
-                }, 1000);
+                if ($scope.workingcalendars.length > 0 && !$scope.currentWorkingCalendar.id) {
+                    $scope.currentWorkingCalendar = $scope.workingcalendars[0];
+                    $timeout(function () {
+                        $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
+                    }, 500);
+                }
             } else {
-                $scope.sensors = [];
-            }
-        });
-
-    };
-
-    $scope.addNonWorkingDay = function () {
-        var nonWorkingDay = {'workingCalendarID': $scope.workingCalendar.workingCalendarID, 'date_local': $scope.date_local};
-        let headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
-        WorkingCalendarNonWorkingDayService.addNonWorkingDay(nonWorkingDay, headers, function (response) {
-            if (angular.isDefined(response.status) && response.status === 201) {
-                toaster.pop({
-                    type: "success",
-                    title: $translate.instant("TOASTER.SUCCESS_TITLE"),
-                    body: $translate.instant('TOASTER.BIND_NON_WORKING_DAY_SUCCESS'),
-                    showCloseButton: true,
-                });
-                $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
-            } else {
-                toaster.pop({
-                    type: "error",
-                    title: $translate.instant(response.data.title),
-                    body: $translate.instant(response.data.description),
-                    showCloseButton: true,
-                });
+                $scope.workingcalendars = [];
             }
         });
     };
-
-    $scope.deleteNonWorkingDay= function (dragEl, dropEl) {
-        if (angular.element('#' + dragEl).hasClass('source')) {
-            return;
-        }
-        var nonWorkingDayID = angular.element('#' + dragEl).scope().nonWorkingDay.id;
-        let headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
-        WorkingCalendarNonWorkingDayService.deleteNonWorkingDay(nonWorkingDayID, headers, function (response) {
-            if (angular.isDefined(response.status) && response.status === 204) {
-                toaster.pop({
-                    type: "success",
-                    title: $translate.instant("TOASTER.SUCCESS_TITLE"),
-                    body: $translate.instant("TOASTER.UNBIND_NON_WORKING_DAY_SUCCESS"),
-                    showCloseButton: true,
-                });
-                $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
-            } else {
-                toaster.pop({
-                    type: "error",
-                    title: $translate.instant(response.data.title),
-                    body: $translate.instant(response.data.description),
-                    showCloseButton: true,
-                });
-            }
-        });
-    };
-
-    $scope.handleDays = function() {
-        $scope.days = [];
-        $scope.nonWorkingDaysFlagArray = [];
-        let non_working_days = $scope.nonworkingdays.map((item) => {return item.date_local})
-        let range = moment($scope.month).endOf('month').date();
-        for(let i = 0; i < range; i++){
-          let date = moment($scope.month).startOf('month').add(i, 'd').format('YYYY-MM-DD');
-          $scope.days.push(date)
-          if(non_working_days.indexOf(date) != -1) {
-            $scope.nonWorkingDaysFlagArray.push(true);
-          }else{
-            $scope.nonWorkingDaysFlagArray.push(false);
-          }
-        }
-      }
-
-    $scope.changeMonth = function(num) {
-        $scope.month = moment($scope.month).add(num, 'months').format('YYYY-MM');
-        $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
-    }
 
     $scope.addNonWorkingDay = function() {
 		var modalInstance = $uibModal.open({
@@ -158,11 +92,88 @@ app.controller('WorkingCalendarNonWorkingDayController', function (
 					});
 				}
 			});
-		}, function() {
-
-		});
+		}, function() {});
         $rootScope.modalInstance = modalInstance;
 	};
+
+    $scope.batchAddWeekendNonWorkingDay = function() {
+        var modalInstance = $uibModal.open({
+            templateUrl: 'views/settings/workingcalendar/batch-weekend-nonworkingday.model.html',
+            controller: 'ModalBatchAddWeekendNonWorkingDayCtrl',
+            windowClass: "animated fadeIn",
+            resolve: {
+                params: function() {
+                    return {
+                        workingCalendar: angular.copy($scope.currentWorkingCalendar),
+                    };
+                }
+            }
+        });
+
+        modalInstance.result.then(function(batchParams) {
+            $scope.handleBatchAddWeekend(batchParams);
+        }, function() {});
+        $rootScope.modalInstance = modalInstance;
+    };
+
+    $scope.handleBatchAddWeekend = function(params) {
+        const startDate = moment(params.startDate);
+        const endDate = moment(params.endDate);
+        const workingCalendarId = params.workingCalendarId;
+        const description = params.description;
+        const headers = { "User-UUID": $scope.cur_user.uuid, "Token": $scope.cur_user.token };
+
+        if (startDate.isAfter(endDate)) {
+            toaster.pop({type: "error", title: $translate.instant("TOASTER.ERROR_ADD_BODY"), body: $translate.instant("SETTING.START_DATE_AFTER_END_DATE"), showCloseButton: true});
+            return;
+        }
+
+        const weekendDates = [];
+        let currentDate = startDate.clone();
+        while (currentDate.isSameOrBefore(endDate)) {
+            const day = currentDate.day();
+            if (day === 0 || day === 6) {
+                weekendDates.push(currentDate.format('YYYY-MM-DD'));
+            }
+            currentDate.add(1, 'day');
+        }
+
+        if (weekendDates.length === 0) {
+            toaster.pop({type: "error", title: $translate.instant("TOASTER.ERROR_ADD_BODY"), body: $translate.instant("SETTING.NO_WEEKEND_IN_RANGE"), showCloseButton: true});
+            return;
+        }
+
+        let successCount = 0;
+        let failCount = 0;
+        const total = weekendDates.length;
+
+        weekendDates.forEach(date => {
+            const nonWorkingDay = {
+                working_calendar_id: workingCalendarId,
+                date_local: date,
+                description: description
+            };
+
+            WorkingCalendarNonWorkingDayService.addNonWorkingDay(workingCalendarId, nonWorkingDay, headers, function (response) {
+                if (response.status === 201) {
+                    successCount++;
+                } else {
+                    failCount++;
+                }
+
+                if (successCount + failCount === total) {
+                    const isSuccess = successCount > 0;
+                    toaster.pop({
+                        type: isSuccess ? "success" : "error",
+                        title: $translate.instant(isSuccess ? "TOASTER.SUCCESS_TITLE" : "TOASTER.ERROR_ADD_BODY"),
+                        body: $translate.instant("SETTING.BATCH_ADD_RESULT", {success: successCount, fail: failCount, total: total}),
+                        showCloseButton: true,
+                    });
+                    $scope.getNonWorkingDaysByWorkingCalendarID(workingCalendarId);
+                }
+            });
+        });
+    };
 
     $scope.editNonWorkingDay = function(nonWorkingDay) {
 		var modalInstance = $uibModal.open({
@@ -199,9 +210,7 @@ app.controller('WorkingCalendarNonWorkingDayController', function (
 					});
 				}
 			});
-		}, function() {
-			//do nothing;
-		});
+		}, function() {});
         $rootScope.modalInstance = modalInstance;
 	};
 
@@ -242,6 +251,26 @@ app.controller('WorkingCalendarNonWorkingDayController', function (
 			});
 	};
 
+    $scope.handleDays = function() {
+        $scope.days = [];
+        $scope.nonWorkingDaysFlagArray = [];
+        let non_working_days = $scope.nonworkingdays.map((item) => {return item.date_local})
+        let range = moment($scope.month).endOf('month').date();
+        for(let i = 0; i < range; i++){
+          let date = moment($scope.month).startOf('month').add(i, 'd').format('YYYY-MM-DD');
+          $scope.days.push(date)
+          if(non_working_days.indexOf(date) != -1) {
+            $scope.nonWorkingDaysFlagArray.push(true);
+          }else{
+            $scope.nonWorkingDaysFlagArray.push(false);
+          }
+        }
+      }
+
+    $scope.changeMonth = function(num) {
+        $scope.month = moment($scope.month).add(num, 'months').format('YYYY-MM');
+        $scope.getNonWorkingDaysByWorkingCalendarID($scope.currentWorkingCalendar.id);
+    }
 
     $scope.getAllWorkingCalendars();
 
@@ -303,4 +332,34 @@ app.controller('ModalEditNonWorkingDayCtrl', function($scope, $uibModalInstance,
 	$scope.cancel = function() {
 		$uibModalInstance.dismiss('cancel');
 	};
+});
+
+app.controller('ModalBatchAddWeekendNonWorkingDayCtrl', function ($scope, $uibModalInstance, params) {
+    $scope.operation = "SETTING.BATCH_ADD_WEEKEND";
+    $scope.batchParams = {
+        startDate: moment().format('YYYY-MM-DD'),
+        endDate: moment().add(1, 'month').format('YYYY-MM-DD'),
+        description: '',
+        workingCalendarId: params.workingCalendar.id
+    };
+    $scope.workingCalendar = params.workingCalendar;
+
+    $scope.dtOptions = {
+        locale: {
+            format: 'YYYY-MM-DD',
+            applyLabel: "OK",
+            cancelLabel: "Cancel",
+        },
+        timePicker: false,
+        timePicker24Hour: true,
+        singleDatePicker: true,
+    };
+
+    $scope.ok = function() {
+        $uibModalInstance.close($scope.batchParams);
+    };
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
