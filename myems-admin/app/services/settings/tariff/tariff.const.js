b448d0026847974a996cb88710e7f646bfee6de2
nengyuanzhang <13011132526@163.com>
2025-12-22 18:34:22 +0800

    added deep to PEAK_TYPE in myems-admin

--- myems-admin/app/services/settings/tariff/tariff.const.js
+++ myems-admin/app/services/settings/tariff/tariff.const.js
@@ -3,7 +3,8 @@ app.constant('PEAK_TYPE',{
 	toppeak:'COMMON.TARIFF.TOPPEAK',
 	onpeak:'COMMON.TARIFF.ONPEAK',
 	offpeak:'COMMON.TARIFF.OFFPEAK',
-	midpeak:'COMMON.TARIFF.MIDPEAK'
+	midpeak:'COMMON.TARIFF.MIDPEAK',
+	deep:'COMMON.TARIFF.DEEP'
 });
 app.constant('TARIFF_TYPE',{
 	timeofuse:'SETTING.TIMEOFUSE'
