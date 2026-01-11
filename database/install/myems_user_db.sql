1df506417339715dc51affe203c07c0f486e12dc
gongtuo <2209581692@qq.com>
2026-01-11 22:17:54 +0800

    Modify the database

--- database/install/myems_user_db.sql
+++ database/install/myems_user_db.sql
@@ -30,6 +30,7 @@ CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_email_messages`  (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `recipient_name` VARCHAR(128) NOT NULL,
   `recipient_email` VARCHAR(128) NOT NULL,
+  `phone` VARCHAR(20) NULL,  
   `subject` VARCHAR(128) NOT NULL,
   `message` LONGTEXT NOT NULL,
   `attachment_file_name` VARCHAR(128) NULL DEFAULT NULL,
@@ -39,6 +40,7 @@ CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_email_messages`  (
   `status` VARCHAR(32) NOT NULL COMMENT 'new, sent, timeout',
   PRIMARY KEY (`id`));
 CREATE INDEX `tbl_email_messages_index_1` ON `myems_user_db`.`tbl_email_messages` (`status`, `scheduled_datetime_utc`);
+CREATE INDEX `tbl_email_messages_index_2` ON `myems_user_db`.`tbl_email_messages` (`phone`);  -- 新增的手机号索引
 
 -- ---------------------------------------------------------------------------------------------------------------------
 -- Table `myems_user_db`.`tbl_email_message_sessions`
@@ -64,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_users` (
   `uuid` CHAR(36) NOT NULL,
   `display_name` VARCHAR(128) NOT NULL,
   `email` VARCHAR(128) NOT NULL,
+  `phone` VARCHAR(20) NULL,
   `salt` VARCHAR(128) NOT NULL,
   `password` VARCHAR(256) NOT NULL,
   `is_admin` BOOL NOT NULL ,
@@ -72,17 +75,19 @@ CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_users` (
   `account_expiration_datetime_utc` DATETIME NOT NULL,
   `password_expiration_datetime_utc` DATETIME NOT NULL,
   `failed_login_count` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `ui_tbl_users_phone` (`phone` ASC));
 
 -- --------------------------------------------------------------------------------------------------------------------
 -- Example Data for table `myems_user_db`.`tbl_users`
 -- --------------------------------------------------------------------------------------------------------------------
 -- default username: administrator
 -- default password: !MyEMS1
-INSERT INTO `myems_user_db`.`tbl_users`(`id`, `name`, `uuid`, `display_name`, `email`, `salt`, `password`, `is_admin`,
+INSERT INTO `myems_user_db`.`tbl_users`(`id`, `name`, `uuid`, `display_name`, `email`, `phone`, `salt`, `password`, `is_admin`,
  `privilege_id`, `account_expiration_datetime_utc`, `password_expiration_datetime_utc`, `failed_login_count`)
 VALUES
 (1, 'administrator', 'dcdb67d1-6116-4987-916f-6fc6cf2bc0e4', 'Administrator', 'administrator@myems.io',
+ NULL,  
  'adfd6fb6d78d4e3780ebdd6afdec2c3a',
  'bc00df65270b1a72b9ed37136fa95a695896edc8c114391821f5edc6b1bbdbabc3d449962f8d1c7a4ec3f2d0a1a79055623963d88ecb9b778423194ff7b6be42',
  1, NULL, '2099-12-31 16:00:00', '2099-12-31 16:00:00', 0);
@@ -137,9 +142,11 @@ CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_new_users` (
   `uuid` CHAR(36) NOT NULL,
   `display_name` VARCHAR(128) NOT NULL,
   `email` VARCHAR(128) NOT NULL,
+  `phone` VARCHAR(20) NULL,
   `salt` VARCHAR(128) NOT NULL,
   `password` VARCHAR(256) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `ui_tbl_new_users_phone` (`phone` ASC));
 
 -- ---------------------------------------------------------------------------------------------------------------------
 -- Table `myems_user_db`.`tbl_notifications`
@@ -166,9 +173,12 @@ DROP TABLE IF EXISTS `myems_user_db`.`tbl_verification_codes`;
 CREATE TABLE IF NOT EXISTS `myems_user_db`.`tbl_verification_codes`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `recipient_email` VARCHAR(128) NOT NULL,
+  `phone` VARCHAR(20) NULL,
   `verification_code` VARCHAR(128) NOT NULL,
   `created_datetime_utc` DATETIME NOT NULL,
   `expires_datetime_utc` DATETIME NOT NULL,
   PRIMARY KEY (`id`));
 CREATE INDEX `tbl_verirication_codes_index_1`
 ON `myems_user_db`.`tbl_verification_codes` (`recipient_email`, `created_datetime_utc`);
+CREATE INDEX `tbl_verirication_codes_index_2`
+ON `myems_user_db`.`tbl_verification_codes` (`phone`); 
