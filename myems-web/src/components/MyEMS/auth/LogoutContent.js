e0d0b53857ac001f7c05ff6505fb2d49ee4ca7d5
nengyuanzhang <13011132526@163.com>
2026-01-20 17:56:05 +0800

    fixed build errors in myems-web

--- myems-web/src/components/MyEMS/auth/LogoutContent.js
+++ myems-web/src/components/MyEMS/auth/LogoutContent.js
@@ -36,7 +36,7 @@ const LogoutContent = ({ layout, titleTag: TitleTag, t }) => {
           createCookie('token', '', 0);
           createCookie('is_logged_in', false, 0);
         } else {
-          handleAPIError(json, setRedirect, setRedirectUrl, t, toast)
+          toast.error(t(json.description));
         }
       })
       .catch(err => {
