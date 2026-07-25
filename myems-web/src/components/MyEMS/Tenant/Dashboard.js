5ae4cfe1f178e4b70d1aadbadc443b7b64ccefd3
oni <674175742@qq.com>
2026-07-25 16:47:46 +0800

    Optimize the data overview function

--- myems-web/src/components/MyEMS/Tenant/Dashboard.js
+++ myems-web/src/components/MyEMS/Tenant/Dashboard.js
@@ -68,7 +68,6 @@ const Dashboard = ({setRedirect, setRedirectUrl, t}) => {
     subtotals: [],
     subtotals_in_kgce: [],
     subtotals_in_kgco2e: [],
-    increment_rates: [],
     timestamps: [],
     values: []
   });
@@ -77,7 +76,6 @@ const Dashboard = ({setRedirect, setRedirectUrl, t}) => {
     names: [],
     units: [],
     subtotals: [],
-    increment_rates: [],
     timestamps: [],
     values: []
   });
@@ -387,8 +385,7 @@ const Dashboard = ({setRedirect, setRedirectUrl, t}) => {
           </CardSummary>
 
           <CardSummary
-              rate={energyData.increment_rate_in_kgce !== undefined ?
-                  (parseFloat(energyData.increment_rate_in_kgce * 100).toFixed(2) + '%') : null}
+              rate={null}
               title={t("This Month's Consumption CATEGORY VALUE UNIT", {
                 CATEGORY: t('Ton of Standard Coal'),
                 VALUE: null,
@@ -404,7 +401,7 @@ const Dashboard = ({setRedirect, setRedirectUrl, t}) => {
           </CardSummary>
 
           <CardSummary
-              rate={costData.subtotals && costData.subtotals.length > 0 ? '+0.00%' : null}
+              rate={costData.subtotals && costData.subtotals.length > 0 ? null : null}
               title={t("This Month's Costs CATEGORY VALUE UNIT", {
                 CATEGORY: null,
                 VALUE: null,
@@ -420,8 +417,7 @@ const Dashboard = ({setRedirect, setRedirectUrl, t}) => {
           </CardSummary>
 
           <CardSummary
-              rate={energyData.increment_rate_in_kgco2e !== undefined ?
-                  (parseFloat(energyData.increment_rate_in_kgco2e * 100).toFixed(2) + '%') : null}
+              rate={null}
               title={t("This Month's Consumption CATEGORY VALUE UNIT", {
                 CATEGORY: t('Ton of Carbon Dioxide Emissions'),
                 VALUE: null,
