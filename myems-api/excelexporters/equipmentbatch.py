7a42630b9211d6e532b6b913525ed51f7022d138
oni <674175742@qq.com>
2026-08-01 07:35:03 +0800

    Increase carbon emissions and costs

--- myems-api/excelexporters/equipmentbatch.py
+++ myems-api/excelexporters/equipmentbatch.py
@@ -203,6 +203,22 @@ def generate_excel(report, space_name, reporting_start_datetime_local, reporting
             " (" + report['energycategories'][i]['unit_of_measure'] + ")"
         ws[col + '6'].border = f_border
 
+    # Carbon Emissions column header
+    carbon_col = chr(ord('D') + ca_len)
+    ws[carbon_col + '6'].fill = table_fill
+    ws[carbon_col + '6'].font = name_font
+    ws[carbon_col + '6'].alignment = c_c_alignment
+    ws[carbon_col + '6'] = _('Carbon Emissions') + ' (KGCO2E)'
+    ws[carbon_col + '6'].border = f_border
+
+    # Costs column header
+    cost_col = chr(ord('D') + ca_len + 1)
+    ws[cost_col + '6'].fill = table_fill
+    ws[cost_col + '6'].font = name_font
+    ws[cost_col + '6'].alignment = c_c_alignment
+    ws[cost_col + '6'] = _('Costs')
+    ws[cost_col + '6'].border = f_border
+
     current_row_number = 7
     for i in range(0, len(report['equipments'])):
 
@@ -223,6 +239,20 @@ def generate_excel(report, space_name, reporting_start_datetime_local, reporting
             ws[col + str(current_row_number)].border = f_border
             ws[col + str(current_row_number)].alignment = c_c_alignment
             ws[col + str(current_row_number)] = round2(report['equipments'][i]['values'][j], 2)
+
+        # Carbon Emissions value
+        carbon_col = chr(ord('D') + ca_len)
+        ws[carbon_col + str(current_row_number)].font = data_font
+        ws[carbon_col + str(current_row_number)].border = f_border
+        ws[carbon_col + str(current_row_number)].alignment = c_c_alignment
+        ws[carbon_col + str(current_row_number)] = round2(report['equipments'][i].get('carbon_emissions', 0.0), 2)
+
+        # Costs value
+        cost_col = chr(ord('D') + ca_len + 1)
+        ws[cost_col + str(current_row_number)].font = data_font
+        ws[cost_col + str(current_row_number)].border = f_border
+        ws[cost_col + str(current_row_number)].alignment = c_c_alignment
+        ws[cost_col + str(current_row_number)] = round2(report['equipments'][i].get('cost', 0.0), 2)
 
         current_row_number += 1
 
